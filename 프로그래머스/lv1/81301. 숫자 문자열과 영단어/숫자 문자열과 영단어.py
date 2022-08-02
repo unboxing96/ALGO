e19c72d9209ef2f74a0aa2f05371dcb8b15e191e
unboxing96 <102353544+unboxing96@users.noreply.github.com>
2022-08-02 14:40:16 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,24 @@
+dic = {
+    'zero' : '0',
+    'one' : '1',
+    'two' : '2',
+    'three' : '3',
+    'four' : '4',
+    'five' : '5',
+    'six' : '6',
+    'seven' : '7',
+    'eight' : '8',
+    'nine' : '9'
+}
+
+
+def solution(s):
+    
+    for key in dic.keys():
+        if key in s:
+            s = s.replace(key, dic[key])
+
+    answer = int(s)
+    return answer
+
+
