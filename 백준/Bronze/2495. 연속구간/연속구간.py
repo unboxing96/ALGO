45c57e61961681ce871234f222c328c737d3211e
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2022-09-18 23:52:25 +0900

    [Bronze II] Title: 연속구간, Time: 104 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Bronze/2495. 연속구간/연속구간.py
@@ -0,0 +1,16 @@
+for _ in range(3):
+
+    s = input()
+
+    mymax = 1
+    cnt = 1
+
+    for i in range(1,len(s)):
+        if s[i]==s[i-1]:
+            cnt+=1
+        else:
+            mymax=max(cnt,mymax)
+            cnt=1
+            
+    mymax = max(cnt, mymax)
+    print(mymax)
