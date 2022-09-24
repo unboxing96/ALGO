0f5cbefdc065437a08de9c90a25a3a8f29a21f35
unboxing96 <unboxing96@gmail.com>
2022-09-25 00:26:40 +0900

    NEW 카카오

---
+++ 프로그래머스/kakao/2022/1. 성격 유형 검사.py
@@ -0,0 +1,21 @@
+survey = ["AN", "CF", "MJ", "RT", "NA"]
+choices = [5, 3, 2, 7, 5]
+
+
+def solution(survey, choices):
+    
+    # 질문 개수만큼 반복
+    # 점수 카운트 dict를 생성하여, 
+        # "1" : {"R" : 0, "T" : 0},
+        # "2" : {"C" : 0, "F" : 0},
+    # 각 survey 대답의 점수를 기록
+    # dict를 다시 반복하며, 
+        # 큰 값의 키를 answer에 추가
+        # 같으면 ascii로 비교 후 추가
+    
+    
+    
+    
+    
+    answer = ''
+    return answer
