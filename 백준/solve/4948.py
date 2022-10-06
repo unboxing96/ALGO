dba699ea1db9c5885f0a51c6e53b721e7cbf96d1
unboxing96 <unboxing96@gmail.com>
2022-10-06 23:45:15 +0900

    221006

--- 백준/solve/4948.py
+++ 백준/solve/4948.py
@@ -1,19 +1,16 @@
-while True:
-    n = int(input())
-    if n == 0:
-        break
+def getPrimaryNum_Eratos(N):
+    nums = [True] * (N)
+    for i in range(2, int(N**0.5) + 1):
+        if nums[i] == True:
+            for j in range(i+i ,N, i):
+                nums[j] = False
+    return [i for i in range(2, N) if nums[i] == True]
 
-    cnt = 0
-    for i in range(n + 1, 2 * n + 1):
-        if i == 1:
-            continue
-        elif i == 2:
-            cnt += 1
-            continue
-        else:
-            for j in range(2, int(i**0.5) + 1):
-                if i % j == 0:
-                    break
-            else:
-                cnt += 1
-    print(cnt)
+while True:
+    N = int(input())
+    
+    if N == 0:
+        break
+    prime_list = getPrimaryNum_Eratos(2*N + 1)
+    answer_list = [num for num in prime_list if num > N]
+    print(len(answer_list))
