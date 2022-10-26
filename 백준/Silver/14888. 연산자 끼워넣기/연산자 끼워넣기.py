95e30b986753e3bc722332505ad70ff66eb75651
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2022-10-26 22:44:40 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 104 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,36 +1,46 @@
-from itertools import permutations
-
+# 수의 개수 입력받기
 n = int(input())
-nums = list(map(int, input().split()))
-opers = list(map(int, input().split()))
+# 수열 입력받기
+data = list(map(int, input().split()))
+# 연산자 개수 계산
+add, sub, mul, div = map(int, input().split())
 
-op = ["+", "-", "*", "/"]
+# 최댓값과 최솟값 초기화
+max_value = -1e9
+min_value = 1e9
 
-oper_list = []
-for i in range(4):
-    for _ in range(opers[i]):
-        oper_list.append(op[i])
+# dfs 매서드 정의
+def dfs(i, arr):
+  global add, sub, mul, div, max_value, min_value
+  # 주어진 수열을 다 받았을 경우 최댓값과 최솟값 계산
+  if i == n:
+    max_value = max(max_value, arr)
+    min_value = min(min_value, arr)
+  else:
+    # 더하기
+    if add > 0:
+      add -= 1
+      dfs(i+1, arr + data[i])
+      add += 1
+    # 빼기
+    if sub > 0:
+      sub -= 1
+      dfs(i+1, arr - data[i])
+      sub += 1
+    # 곱하기
+    if mul > 0:
+      mul -= 1
+      dfs(i+1, arr * data[i])
+      mul += 1
+    # 나누기
+    if div > 0:
+      div -= 1
+      dfs(i+1, int(arr / data[i]))
+      div += 1
+  
+# DFS 메서드 호출
+dfs(1, data[0])
 
-
-answer = []
-
-oper_set_list = list(set(permutations(oper_list)))
-for oper_set in oper_set_list: # oper_set = ["+", "+", "-", "/", "*"]
-    n = nums[0]
-    for i in range(len(nums)-1): # i = 0, 1, 2, 3, 4
-        if oper_set[i] == "+":
-            n += nums[i+1]
-        elif oper_set[i] == "-":
-            n -= nums[i+1]
-        elif oper_set[i] == "*":
-            n *= nums[i+1]
-        elif oper_set[i] == "/":
-            if n // nums[i+1] < 0:
-                n = -(-n // nums[i+1])
-            else:
-                n //= nums[i+1]
-        
-    answer.append(n)
-
-print(max(answer))
-print(min(answer))
+# 최댓값과 최솟값 출력
+print(max_value)
+print(min_value)
