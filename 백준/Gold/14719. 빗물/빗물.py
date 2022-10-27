005829d7f1082ace3174839358b931a7319df2e4
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2022-10-28 08:51:23 +0900

    [Gold V] Title: 빗물, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -1,22 +1,12 @@
 h, w = map(int, input().split())
 waters = list(map(int, input().split()))
 
-data = []
-for _ in range(h):
-    data.append([0 for _ in range(w)])
+ans = 0
+for i in range(1, w-1):
+    left = max(waters[:i])
+    right = max(waters[i+1:])
+    compare = min(left, right)
+    if waters[i] < compare:
+        ans += compare- waters[i]
 
-
-for i in range(h):
-    for j in range(w):
-        if waters[j]:
-            data[i][j] += 1
-            waters[j] -= 1
-
-cnt = 0
-for i in range(h):
-    for j in range(w):
-        if data[i][j] == 0:
-            if 1 in data[i][:j] and 1 in data[i][j+1:]:
-                cnt += 1
-
-print(cnt)
+print(ans)
