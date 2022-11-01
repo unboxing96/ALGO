3ed35568bd9ee5f6c08ccfaab050644925dbf47a
unboxing96 <unboxing96@gmail.com>
2022-11-01 23:10:21 +0900

    week1 #6

--- 우테코/week_1/06.py
+++ 우테코/week_1/06.py
@@ -1,1 +1,20 @@
-print("제이" in "제이엠")
+def nicks(forms):
+    
+    names = []
+    for form in forms:
+        names.append(form[1])
+
+    result = []
+    for i in range(len(forms)):
+        for j in range(len(forms[i])):
+            for k in range(len(names) - 1):
+                if k == i:
+                    continue
+                if forms[i][1][j:j+2] in names[k]:
+                    result.append(forms[i][0])
+                    break
+    return result
+
+forms = [ ["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"] ]
+result = nicks(forms)
+print(result)
