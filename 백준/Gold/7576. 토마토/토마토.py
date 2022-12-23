e4e8f6a3c38aa1888eeda012d26e45a816c68af8
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2022-12-23 18:01:39 +0900

    [Gold V] Title: 토마토, Time: 1284 ms, Memory: 98564 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,4 +1,3 @@
-
 from collections import deque
 
 m, n = map(int, input().split())
@@ -12,31 +11,26 @@ result = 0
 q = deque()
 
 
-def bfs():
-
-    while q:
-        x, y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
-                graph[nx][ny] = graph[x][y] + 1
-                q.append((nx, ny))
-
-
 for i in range(n):
     for j in range(m):
         if graph[i][j] == 1:
             q.append((i, j))
+def bfs():
+    while q:
+            x, y = q.popleft()
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+
+                if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
+                    graph[nx][ny] = graph[x][y] + 1
+                    q.append((nx, ny))
 
 bfs()
 
-for i in graph:
-    for j in i:
-        if j == 0:
-            print(-1)
-            exit(0)
-    result = max(result, max(i))
-else:
-    print(result - 1)
+result = max(map(max, graph)) - 1
+for row in graph:
+    if 0 in row:
+        result = -1
+
+print(result)
