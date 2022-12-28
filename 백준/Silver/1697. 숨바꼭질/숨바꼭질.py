2f859006ba1e30c6d773d2ab2558744004c19c77
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2022-12-28 12:26:17 +0900

    [Silver I] Title: 숨바꼭질, Time: 100 ms, Memory: 35320 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -7,18 +7,18 @@ def bfs():
 
     while q:
         x = q.popleft()
-        if x == k:
-            return dist[x]
 
-        for nx in (x - 1, x + 1, x * 2):
-            if 0 <= nx <= MAX and dist[nx] == 0:
-                dist[nx] = dist[x] + 1
+        if x == k:
+            return graph[x]
+
+        for nx in (x + 1, x - 1, x * 2):
+            if 0 <= nx <= MAX and graph[nx] == 0:
+                graph[nx] = graph[x] + 1
                 q.append(nx)
 
 
 n, k = map(int, input().split())
-
 MAX = 10**5
-dist = [0] * (MAX + 1)
+graph = [0] * (MAX + 1)
 
 print(bfs())
