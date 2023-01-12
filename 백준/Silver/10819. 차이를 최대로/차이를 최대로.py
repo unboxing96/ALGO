4a39807c56094041c8443d74b98e48dd1d3a7487
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2023-01-12 15:03:41 +0900

    [Silver II] Title: 차이를 최대로, Time: 136 ms, Memory: 37288 KB -BaekjoonHub

---
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
@@ -0,0 +1,16 @@
+
+from itertools import permutations
+
+n = int(input())
+nums = list(map(int, input().split()))
+
+perm = list(permutations(nums, n))
+
+result = []
+for p in perm:
+    tot = 0
+    for i in range(1, n):
+        tot += abs(p[i] - p[i - 1])
+    result.append(tot)
+
+print(max(result))
