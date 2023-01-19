97169f827bf23ac21147a7481a02ee733441a488
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2023-01-19 11:24:16 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 135.40 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/lv2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
+++ 프로그래머스/lv2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
@@ -1,82 +1,39 @@
 
 def solution(rows, columns, queries):
+    answer = []
+    matrix = [[0 for i in range(columns+1)] for j in range(rows+1)]
+    num = 1
+    for row in range(1, rows+1):
+        for column in range(1, columns+1):
+            matrix[row][column] = num
+            num += 1
+            
+    for x1, y1, x2, y2 in queries:
+        tmp = matrix[x1][y1]
+        mini = tmp
+        
+        for k in range(x1,x2):
+            test = matrix[k+1][y1]
+            matrix[k][y1] = test
+            mini = min(mini, test)
 
-    res = []
+        for k in range(y1,y2):
+            test = matrix[x2][k+1]
+            matrix[x2][k] = test
+            mini = min(mini, test)
 
-    # generate graph
-    check = [[False] * (columns + 1) for _ in range(rows + 1)]
+        for k in range(x2,x1,-1):
+            test = matrix[k-1][y2]
+            matrix[k][y2] = test
+            mini = min(mini, test)
 
-    # generate graph
-    graph = [[0] * (columns + 1) for _ in range(rows + 1)]
-    k = 1
-    for i in range(1, rows + 1):
-        for j in range(1, columns + 1):
-            graph[i][j] = k
-            k += 1
+        for k in range(y2,y1,-1):
+            test = matrix[x1][k-1]
+            matrix[x1][k] = test
+            mini = min(mini, test)
 
-    for q in queries:
+        matrix[x1][y1+1] = tmp
+        answer.append(mini)
+            
+    return answer
 
-        # check new numbers
-        visited = [[0] * (columns + 1) for _ in range(rows + 1)]
-
-        x1, y1, x2, y2 = q[0], q[1], q[2], q[3]
-
-        for i in range(x1, x2 + 1):
-            for j in range(y1, y2 + 1):
-                # 첫 행
-                if i == x1:
-                    if j == y2:
-                        if not visited[i + 1][j]:
-                            visited[i + 1][j] = graph[i][j]
-                            check[i + 1][j] = True
-                    else:
-                        if not visited[i][j + 1]:
-                            visited[i][j + 1] = graph[i][j]
-                            check[i][j + 1] = True
-                # 끝 열
-                if j == y2:
-                    if i == x2:
-                        if not visited[i][j - 1]:
-                            visited[i][j - 1] = graph[i][j]
-                            check[i][j - 1] = True
-                    else:
-                        if not visited[i + 1][j]:
-                            visited[i + 1][j] = graph[i][j]
-                            check[i + 1][j] = True
-
-                # 끝 행
-                if i == x2:
-                    if j == y1:
-                        if not visited[i - 1][j]:
-                            visited[i - 1][j] = graph[i][j]
-                            check[i - 1][j] = True
-                    else:
-                        if not visited[i][j - 1]:
-                            visited[i][j - 1] = graph[i][j]
-                            check[i][j - 1] = True
-
-                # 첫 열
-                if j == y1:
-                    if i == x1:
-                        if not visited[i][j + 1]:
-                            visited[i][j + 1] = graph[i][j]
-                            check[i][j + 1] = True
-                    else:
-                        if not visited[i - 1][j]:
-                            visited[i - 1][j] = graph[i][j]
-                            check[i - 1][j] = True
-
-        for i in range(1, rows + 1):
-            for j in range(1, columns + 1):
-                if visited[i][j]:
-                    graph[i][j] = visited[i][j]
-
-        bottom = 1e9
-        for i in range(x1, x2 + 1):
-            for j in range(y1, y2 + 1):
-                if visited[i][j] and visited[i][j] < bottom:
-                    bottom = visited[i][j]
-
-        res.append(bottom)
-
-    return res
