e11c70ce1557b0d1845908a32ddec2d5ddb127c1
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2023-02-03 21:38:52 +0900

    [Silver II] Title: DFS와 BFS, Time: 212 ms, Memory: 117784 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,46 +1,40 @@
-n, m, v = map(int, input().split())
-
-graph = [[0] * (n + 1) for i in range(n + 1)]
-visited = [0] * (n + 1)
-
-# 인접 행렬 생성 (edge만큼 반복)
-for i in range(m):
-    a, b = map(int, input().split())
-    graph[a][b] = 1
-    graph[b][a] = 1
+from collections import deque
 
 
 def dfs(v):
-    # 방문한 노드 += 1
-    visited[v] = 1
-
+    visited[v] = True
     print(v, end=" ")
-
-    # 행렬과 방문 노드 순회
-    for i in range(1, n + 1):
-        # 인접 행렬이지만, 아직 방문하지 않은 노드일 때
-        if graph[v][i] == 1 and visited[i] == 0:
+    for i in graph[v]:
+        if not visited[i]:
             dfs(i)
 
 
 def bfs(v):
-    # 방문해야 할 곳을 순서대로 넣을 큐
-    q = [v]
-
-    # 방문한 곳은 visited를 다시 0으로
-    visited[v] = 0
+    q.append(v)
+    visited[v] = False
 
     while q:
-        # 큐에서 처음 올린 노드 꺼내줌
-        v = q.pop(0)
-        print(v, end=" ")
+        x = q.popleft()
+        print(x, end=" ")
 
-        # 행렬과 방문 노드 순회
-        for i in range(1, n + 1):
-            if graph[v][i] == 1 and visited[i] == 1:
+        for i in graph[x]:
+            if visited[i]:
                 q.append(i)
-                visited[i] = 0
+                visited[i] = False
 
+
+n, m, v = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+visited = [False] * (n + 1)
+q = deque()
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+for i in range(1, n+1):
+    graph[i].sort()
 
 dfs(v)
 print("")
