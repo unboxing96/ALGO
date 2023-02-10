e382ab2a92d6d4151862f7d4c2a6ad82801d899e
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2023-02-10 14:14:13 +0900

    [level 3] Title: 징검다리 건너기, Time: 215.74 ms, Memory: 18.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -0,0 +1,24 @@
+
+
+def solution(stones, k):
+
+    left = 1
+    right = 200000000
+
+    while left <= right:
+        mid = (left + right) // 2
+        cnt = 0
+        for stone in stones:
+            if stone - mid <= 0:
+                cnt += 1
+                if cnt == k:
+                    break
+            else:
+                cnt = 0
+
+        if cnt >= k:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return left
