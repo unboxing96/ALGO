f3509160df51a08afa70c076ff63219132006352
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2023-02-16 14:09:15 +0900

    [level 3] Title: 징검다리 건너기, Time: 176.72 ms, Memory: 18.6 MB -BaekjoonHub

--- 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -1,29 +1,24 @@
 def solution(stones, k):
-
-	# 이분 탐색의 두 포인터를 '건넌 사람 수'를 기준으로
-    left = 1
-    right = max(stones) # 문제에서 주어진 최대값 200000000 설정하면 더 빠름
-	
-    # 왼쪽 포인터가 오른쪽에 닿을 때까지
+    left, right = 1, max(stones)
+    
     while left <= right:
-        cnt = 0
         mid = (left + right) // 2
-		
-        # 선형 탐색하며, 현재 값에서 mid를 빼주고, 설정한 mid 값이 최적인지 판단
-        for stone in stones:
+        cnt = 0
+        flag = False
         
-        	# 현재값 - mid가 0 이하이면 == 징검다리가 0이 되면
+        for stone in stones:
             if stone - mid <= 0:
                 cnt += 1
-                # 연속된 0의 개수가 k 이상이 되면
-                if cnt >= k:
-                    break
             else:
                 cnt = 0
-
-        if cnt >= k:
+            
+            if cnt == k:
+                flag = True
+                break
+        
+        if flag:
             right = mid - 1
         else:
             left = mid + 1
-
+    
     return left
