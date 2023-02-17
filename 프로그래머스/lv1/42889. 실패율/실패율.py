22981cfee5bf9f77b6f0d2d004e516cca9eafb8c
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2023-02-17 14:44:56 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.py
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -1,17 +1,11 @@
-# calculate failure rate of each stage
-
-
 def solution(N, stages):
-    answer = []
-    total = len(stages)
+    size = len(stages)
+    dic = {}
     for i in range(1, N + 1):
-        count = stages.count(i)
-        if total == 0:
-            fail = 0
+        num = stages.count(i)
+        if size == 0:
+            dic[i] = 0
         else:
-            fail = count / total
-        answer.append((i, fail))
-        total -= count
-    answer = sorted(answer, key=lambda t: t[1], reverse=True)
-    answer = [i[0] for i in answer]
-    return answer
+            dic[i] = num / size
+        size -= num
+    return sorted(dic, key=lambda x: dic[x], reverse=True)
