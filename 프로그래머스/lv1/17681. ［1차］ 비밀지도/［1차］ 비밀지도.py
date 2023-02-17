e1e965a187d54586c0efab6208339fdffd6eaccc
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2023-02-17 13:02:52 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,17 +1,9 @@
 def solution(n, arr1, arr2):
 
-    new = []
-    for i in range(n):
-        new.append(arr1[i] | arr2[i])
-
-    last = []
-    for i in range(n):
-        last.append(bin(new[i])[2:].rjust(n, "0"))
-
     result = []
     for i in range(n):
-        res = last[i].replace("1", "#")
-        res = res.replace("0", " ")
-        result.append(res)
+        new = bin(arr1[i] | arr2[i])[2:].rjust(n, "0")
+        new = new.replace("1", "#")
+        result.append(new.replace("0", " "))
 
     return result
