0a233ca11277535b7404a80c7ede2b4b3ec2fdaa
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2023-02-18 15:55:34 +0900

    [Gold V] Title: 인구 이동, Time: 772 ms, Memory: 156120 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -1,48 +1,75 @@
-from collections import deque
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.size = [1] * n
+
+    def find(self, x):
+        if x != self.parent[x]:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        x_root, y_root = self.find(x), self.find(y)
+        if x_root != y_root:
+            if self.size[x_root] < self.size[y_root]:
+                x_root, y_root = y_root, x_root
+            self.parent[y_root] = x_root
+            self.size[x_root] += self.size[y_root]
+            self.size[y_root] = 0
+
 
 n, l, r = map(int, input().split())
 countries = [list(map(int, input().split())) for _ in range(n)]
 
-def bfs(start, visited):
-    queue = deque([start])
-    union = [start]
-    total_population = countries[start[0]][start[1]]
+def get_union_population(unions):
+    total_population = 0
+    for union in unions:
+        population = 0
+        for i, j in union:
+            population += countries[i][j]
+        total_population += population // len(union)
+        for i, j in union:
+            countries[i][j] = population // len(union)
+    return total_population
 
-    while queue:
-        x, y = queue.popleft()
-        for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-            nx, ny = x + dx, y + dy
-            if nx < 0 or nx >= n or ny < 0 or ny >= n:
-                continue
-            if (nx, ny) in visited:
-                continue
-            population_diff = abs(countries[x][y] - countries[nx][ny])
-            if l <= population_diff <= r:
-                visited.add((nx, ny))
-                queue.append((nx, ny))
-                union.append((nx, ny))
-                total_population += countries[nx][ny]
-    return union, total_population
+
+def get_unions():
+    unions = []
+    uf = UnionFind(n*n)
+
+    for i in range(n):
+        for j in range(n):
+            if j < n-1:
+                diff = abs(countries[i][j] - countries[i][j+1])
+                if l <= diff <= r:
+                    uf.union(i*n+j, i*n+(j+1))
+
+            if i < n-1:
+                diff = abs(countries[i][j] - countries[i+1][j])
+                if l <= diff <= r:
+                    uf.union(i*n+j, (i+1)*n+j)
+
+    roots = {}
+    for i in range(n):
+        for j in range(n):
+            root = uf.find(i*n+j)
+            if root not in roots:
+                roots[root] = []
+            roots[root].append((i, j))
+
+    for root in roots.values():
+        if len(root) > 1:
+            unions.append(root)
+
+    return unions
+
 
 days = 0
 while True:
-    visited = set()
-    unions = []
-    for i in range(n):
-        for j in range(n):
-            if (i, j) not in visited:
-                visited.add((i, j))
-                union, total_population = bfs((i, j), visited)
-                if len(union) > 1:
-                    unions.append((union, total_population))
-
+    unions = get_unions()
     if not unions:
         break
-
-    for union, total_population in unions:
-        population_per_country = total_population // len(union)
-        for x, y in union:
-            countries[x][y] = population_per_country
+    get_union_population(unions)
     days += 1
 
 print(days)
