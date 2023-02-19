075edeea2e27f0436f16780ed0ab41f596878a0d
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2023-02-19 14:06:21 +0900

    [level 3] Title: 합승 택시 요금, Time: 299.76 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,21 +1,28 @@
 def solution(n, s, a, b, fares):
-    answer = 10 ** 9
-    cost = [[answer] * (n+1) for _ in range(n+1)]
-    
-    def floyd():
-        for k in range(n+1):
-            for i in range(n+1):
-                for j in range(n+1):
-                    if i == j:
-                        cost[i][j] = 0
-                    else:
-                        cost[i][j] = min(cost[i][j], cost[i][k] + cost[k][j])
-                        
-    for i, j, c in fares:
-        cost[i][j] = c
-        cost[j][i] = c
-    floyd()
-    
-    for i in range(1, n+1):
-        answer = min(answer, cost[s][i] + cost[i][a] + cost[i][b])
+
+    # 거리 초기화
+    INF = int(1e9)
+    dist = [[INF] * (n + 1) for _ in range(n + 1)]
+    answer = INF
+
+    # 자기 자신 0으로 초기화
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if i == j:
+                dist[i][j] = 0
+
+    # 가중치 초기화
+    for i, j, cost in fares:
+        dist[i][j] = dist[j][i] = cost
+
+    # 최소 거리로 초기화
+    for k in range(1, n + 1):
+        for i in range(1, n + 1):
+            for j in range(1, n + 1):
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+
+    for i in range(1, n + 1):
+        cost = dist[s][i] + dist[i][a] + dist[i][b]
+        answer = min(answer, cost)
+
     return answer
