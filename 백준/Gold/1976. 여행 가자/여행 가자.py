e1317f54f7a4f1e391ed07d9c507d51006513747
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2023-02-19 13:24:11 +0900

    [Gold IV] Title: 여행 가자, Time: 132 ms, Memory: 115396 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.py
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -1,14 +1,16 @@
+n = int(input())
+m = int(input())
 
-# union-find algorithm
-def find_parent(parent, x):
+
+def find(x):
     if parent[x] != x:
-        parent[x] = find_parent(parent, parent[x])
+        parent[x] = find(parent[x])
     return parent[x]
 
 
-def union_parent(parent, a, b):
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
+def union(a, b):
+    a = find(a)
+    b = find(b)
 
     if a < b:
         parent[b] = a
@@ -16,24 +18,22 @@ def union_parent(parent, a, b):
         parent[a] = b
 
 
-# union-find
-n = int(input())
-m = int(input())
+# union - find
 parent = [i for i in range(n)]
+mat = [list(map(int, input().split())) for _ in range(n)]
+
 for i in range(n):
-    link = list(map(int, input().split()))
     for j in range(n):
-        if link[j] == 1:
-            union_parent(parent, i, j)
+        if mat[i][j] == 1:
+            union(i, j)
 
-
-# 경로 체크
-parent = [-1] + parent
+# 끊어진 연결 확인하기
 path = list(map(int, input().split()))
+parent = [-1] + parent
 start = parent[path[0]]
 
 for i in range(1, m):
-    if parent[path[i]] != start:
+    if start != parent[path[i]]:
         print("NO")
         break
 else:
