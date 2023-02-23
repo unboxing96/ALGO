f5c0c9c0055697b574cc4f8616a04e3d7b4a5dc7
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2023-02-23 14:51:15 +0900

    [Silver I] Title: 미로 탐색, Time: 160 ms, Memory: 116588 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,44 +1,30 @@
 from collections import deque
 
-N, M = map(int, input().split())
+n, m = map(int, input().split())
+graph = [list(map(int, input())) for _ in range(n)]
+visited = [[False] * m for _ in range(n)]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-graph = []
 
-for _ in range(N):
-  graph.append(list(map(int, input())))
+def bfs():
+    visited[0][0] = True
+    q = deque()
+    q.append((0, 0))
 
-# 너비 우선 탐색
-def bfs(x, y):
-  # 이동할 네 가지 방향 정의 (상, 하, 좌, 우)
-  dx = [-1, 1, 0, 0] 
-  dy = [0, 0, -1, 1]
+    while q:
+        x, y = q.popleft()
 
-  # deque 생성
-  queue = deque()
-  queue.append((x, y))
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
 
-  while queue:
-    x, y = queue.popleft()
-    
-    # 현재 위치에서 4가지 방향으로 위치 확인
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
+                if graph[nx][ny] == 1:
+                    visited[nx][ny] = True
+                    graph[nx][ny] = graph[x][y] + 1
+                    q.append((nx, ny))
 
-      # 위치가 벗어나면 안되기 때문에 조건 추가
-      if nx < 0 or nx >= N or ny < 0 or ny >= M:
-        continue
-      
-      # 벽이므로 진행 불가
-      if graph[nx][ny] == 0:
-        continue
-      
-      # 벽이 아니므로 이동
-      if graph[nx][ny] == 1:
-        graph[nx][ny] = graph[x][y] + 1
-        queue.append((nx, ny))
-  
-  # 마지막 값에서 카운트 값을 뽑는다.
-  return graph[N-1][M-1]
 
-print(bfs(0, 0))
+bfs()
+print(graph[n - 1][m - 1])
