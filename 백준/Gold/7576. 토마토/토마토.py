b9aa90bf6b7b7e501b15ee5ac1b6f8150fdf1d13
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2023-02-24 15:23:20 +0900

    [Gold V] Title: 토마토, Time: 476 ms, Memory: 167092 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -2,35 +2,38 @@ from collections import deque
 
 m, n = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
-
+visited = [[False] * m for _ in range(n)]
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-
-result = 0
-
 q = deque()
-
+res = 0
 
 for i in range(n):
     for j in range(m):
         if graph[i][j] == 1:
             q.append((i, j))
-def bfs():
-    while q:
-            x, y = q.popleft()
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
 
-                if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
-                    graph[nx][ny] = graph[x][y] + 1
-                    q.append((nx, ny))
+
+def bfs():
+
+    while q:
+        x, y = q.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
+                graph[nx][ny] = graph[x][y] + 1
+                q.append((nx, ny))
+
 
 bfs()
 
-result = max(map(max, graph)) - 1
 for row in graph:
-    if 0 in row:
-        result = -1
-
-print(result)
+    for elem in row:
+        if elem == 0:
+            print(-1)
+            exit(0)
+    res = max(res, max(row))
+print(res - 1)
