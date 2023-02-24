dde35fbc887d929421db47ad47ea20296aedbd24
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2023-02-24 16:35:39 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 596 ms, Memory: 180832 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,27 +1,35 @@
+
+from collections import deque
+
 n, m = map(int, input().split())
 
 graph = [[] for _ in range(n + 1)]
-
-visited = [0] * (n + 1)
+visited = [False] * (n + 1)
 
 for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-def dfs(start):
 
-    visited[start] = 1
+def bfs(start):
+    visited[start] = True
+    q = deque()
+    q.append(start)
 
-    for i in graph[start]:
-        if visited[i] == 0:
-            dfs(i)
+    while q:
+        x = q.popleft()
+
+        for i in graph[x]:
+            if not visited[i]:
+                visited[i] = True
+                q.append(i)
+
 
 cnt = 0
-
-for i in range(1, n+1):
-    if visited[i] == 0:
-        dfs(i)
+for i in range(1, n + 1):
+    if not visited[i]:
+        bfs(i)
         cnt += 1
-        
+
 print(cnt)
