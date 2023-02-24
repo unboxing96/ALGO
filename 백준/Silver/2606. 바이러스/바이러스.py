4bc50fbaf5cc249d33e2053d045be48b6e4c2eb6
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2023-02-24 10:32:12 +0900

    [Silver III] Title: 바이러스, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,26 +1,35 @@
-n = int(input())
-m = int(input())
+N = int(input()) # 노드
+M = int(input()) # 엣지
 
-visited = [0] * (n + 1)
+# 인접 리스트 생성
+adj = [[] for _ in range(N+1)]
 
-graph = [ [] * n for _ in range(n+1)]
+for i in range(M):
+    u, v = map(int, input().split())
+    adj[u].append(v)
+    adj[v].append(u)
 
-for i in range(m):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
+# 감염시키면 1, 아니면 0
+result = [0 for _ in range(N+1)]
 
+# 감염 대수, 재귀를 위해 함수 바깥에
 cnt = 0
 
-# DFS
-def DFS(start):
-    global cnt
-    visited[start] = 1
+def virus(index):
+    # 글로벌 변수 가져옴
+    global cnt                      # 없으면: UnboundLocalError: local variable 'cnt' referenced before assignment
+    
+    # 감염된 컴퓨터 위치를, result에 표시
+    result[index] = 1               # +=1이 아닌 이유: True(1) / False(0)를 따지는 것이기 때문에
 
-    for i in graph[start]:
-        if visited[i] == 0:
-            DFS(i)
-            cnt += 1
+    
+    for i in adj[index]:            # 자식 리스트 순회하며
+        if result[i] == 0:          # 감염 여부 리스트에 표시된 적 없으면
+            virus(i)                # 함수 재귀 후
+            cnt += 1                # cnt 세어줌
+    
+    return True
 
-DFS(1)
+virus(1)                            # 문제 조건
+
 print(cnt)
