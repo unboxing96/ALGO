c712992e675e0aa7bb00b31feae15128d96024cf
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2023-02-24 14:21:37 +0900

    [Silver II] Title: 유기농 배추, Time: 208 ms, Memory: 116460 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,42 +1,42 @@
-import sys
-sys.setrecursionlimit(10000)
 
-t = int(input())
+from collections import deque
 
-for tc in range(t):
+tc = int(input())
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-    n, m, k = map(int, input().split())
 
-    graph = [[0] * m for _ in range(n)]
+def bfs(x, y):
+    graph[x][y] = 0
+    q = deque()
+    q.append((x, y))
 
-    for i in range(k):
-        a, b = map(int, input().split())
-        graph[a][b] = 1
+    while q:
+        x, y = q.popleft()
 
-    result = 0
-
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-
-    def dfs(x, y):
-        # 방문 처리
-        graph[x][y] = 0
-
-        # 4방 탐색
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                continue
+            if 0 <= nx < n and 0 <= ny < m:
+                if graph[nx][ny] == 1:
+                    graph[nx][ny] = 0
+                    q.append((nx, ny))
 
-            if graph[nx][ny] == 1:
-                dfs(nx, ny)
 
+for _ in range(tc):
+    n, m, e = map(int, input().split())
+    graph = [[0] * m for _ in range(n)]
+
+    for _ in range(e):
+        a, b = map(int, input().split())
+        graph[a][b] = 1
+
+    time = 0
     for i in range(n):
         for j in range(m):
             if graph[i][j] == 1:
-                dfs(i, j)
-                result += 1
+                bfs(i, j)
+                time += 1
 
-    print(result)
+    print(time)
