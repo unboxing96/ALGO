3f12f33196178ab147179b2ca6dc03d22c46e354
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2023-03-03 10:28:43 +0900

    [Gold II] Title: 퍼즐, Time: 448 ms, Memory: 168620 KB -BaekjoonHub

--- 백준/Gold/1525. 퍼즐/퍼즐.py
+++ 백준/Gold/1525. 퍼즐/퍼즐.py
@@ -1,44 +1,44 @@
 from collections import deque
 
-# 퍼즐을 문자열 123456780로 정렬시킨다고 생각하자
+# 2차원 행렬을 문자열로 바꿔서 판단
 puzzle = ""
-for i in range(3):
+for _ in range(3):
     puzzle += "".join(list(input().split()))
 
-# 현재 puzzle의 모습을 key로, 움직인 횟수를 value로 저장
-visited = {puzzle:0}
+# 그리고 해당 문자열을 key로, 이동 횟수를 value로 하는 딕셔너리 생성
+dist = {puzzle: 0}
 q = deque([puzzle])
 
-# 상하좌우로 0(빈칸)을 이동
+# 델타 좌표
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
+
+
 def bfs():
     while q:
         puzzle = q.popleft()
-        cnt = visited[puzzle]
-        z = puzzle.index('0') # 0(빈칸)의 위치
-        
-        if puzzle == '123456780':
-            return cnt
-        
-        x = z // 3 # 2차원 배열의 행
-        y = z % 3 # 2차원 배열의 열
-        
+        z = puzzle.index("0")
+
+        if puzzle == "123456780":
+            return dist[puzzle]
+
+        x = z // 3
+        y = z % 3
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx <= 2 and 0 <= ny <= 2: # 이동 가능한 위치일 경우
-                # nx, ny를 다시 리스트의 인덱스로 바꾸자
+
+            if 0 <= nx < 3 and 0 <= ny < 3:
                 nz = nx * 3 + ny
-                puzzle_list = list(puzzle) # 원소 스와핑을 위해 문자열을 리스트로 바꾸자
+                puzzle_list = list(puzzle)
                 puzzle_list[z], puzzle_list[nz] = puzzle_list[nz], puzzle_list[z]
-                new_puzzle = "".join(puzzle_list) # 딕셔너리를 위해 다시 문자열로
-                
-                # 방문하지 않았다면
-                if visited.get(new_puzzle, 0) == 0:
-                    visited[new_puzzle] = cnt + 1
+                new_puzzle = "".join(puzzle_list)
+
+                if dist.get(new_puzzle, 0) == 0:
+                    dist[new_puzzle] = dist[puzzle] + 1
                     q.append(new_puzzle)
-                    
     return -1
-                    
+
+
 print(bfs())
