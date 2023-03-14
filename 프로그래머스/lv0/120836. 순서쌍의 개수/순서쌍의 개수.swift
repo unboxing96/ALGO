91c4aa68ad22080d331d068ec2351ad4ddc51c06
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2023-03-14 17:25:01 +0900

    [level 0] Title: 순서쌍의 개수, Time: 14.72 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    
+    var ans = 0
+    
+    for i in 1...n {
+        if n % i == 0 {
+            ans += 1
+        }
+    }
+    
+    return ans
+}
