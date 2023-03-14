217d427e75d55c0cac697389dc044dc361f5e377
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2023-03-14 20:28:15 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func solution(_ str1:String, _ str2:String) -> Int {
+    
+    if str1 == str2 {
+        return 1
+    }
+    
+    if str1.contains(str2) {
+        return 1
+    } else {
+        return 2
+    }
+}
