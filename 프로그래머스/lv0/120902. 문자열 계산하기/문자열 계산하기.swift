81a747046e540f65b0f5afd0b1d5c8380f2cc6ce
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2023-03-15 22:36:52 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.12 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.swift
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.swift
@@ -1,20 +1,5 @@
-func solution(_ my_string: String) -> Int {
-    let components = my_string.components(separatedBy: " ")
-    var result = Int(components[0])!
-    
-    for i in stride(from: 1, to: components.count, by: 2) {
-        let operatorString = components[i]
-        let operand = Int(components[i+1])!
-        
-        switch operatorString {
-        case "+":
-            result += operand
-        case "-":
-            result -= operand
-        default:
-            break
-        }
-    }
-    
-    return result
+import Foundation
+
+func solution(_ my_string:String) -> Int {
+    return 0
 }
