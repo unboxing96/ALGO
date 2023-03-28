1e041865a2f3f1d37cdbb33ec661179971372d30
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2023-03-28 20:09:38 +0900

    [Unrated] Title: 2×n 타일링, Time: 168 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Unrated/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+dp = [0] * 1001
+dp[1] = 1
+dp[2] = 2
+
+
+def fibo(x):
+
+    for i in range(3, x+1):
+        dp[i] = dp[i-1] + dp[i-2]
+    
+    return dp[x]
+
+print(fibo(n) % 10007)
