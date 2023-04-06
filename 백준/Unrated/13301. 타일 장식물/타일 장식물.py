9cd4be7e86cd04709b8cf7e8ee8c675bf54f8594
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2023-04-06 16:10:33 +0900

    [Unrated] Title: 타일 장식물, Time: 116 ms, Memory: 241112 KB -BaekjoonHub

---
+++ 백준/Unrated/13301. 타일 장식물/타일 장식물.py
@@ -0,0 +1,19 @@
+import sys
+sys.setrecursionlimit(10 ** 5)
+
+n = int(input())
+
+dp = [0] * 10001
+
+def fibo(x):
+    if x == 0 or x == 1:
+        return x
+    
+    elif dp[x]:
+        return dp[x]
+    
+    dp[x] = fibo(x - 2) + fibo(x - 1)
+    return dp[x]
+
+
+print(2 * (fibo(n + 1) + fibo(n)))
