bb6722d66dca6611997060de8ee1eca1aa54c23c
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2023-04-09 09:53:21 +0900

    [Gold V] Title: 토마토, Time: 432 ms, Memory: 159100 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -2,20 +2,18 @@ from collections import deque
 
 m, n = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
-visited = [[False] * m for _ in range(n)]
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 q = deque()
-res = 0
 
+# draw graph, search graph to find 1 and append to deque
 for i in range(n):
     for j in range(m):
         if graph[i][j] == 1:
             q.append((i, j))
 
-
+# 4 directions delta search with BFS,
 def bfs():
-
     while q:
         x, y = q.popleft()
 
@@ -23,17 +21,26 @@ def bfs():
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            
+            # where is not visited and not -1
+            if graph[nx][ny] == 0:
+
+                # record the day on graph with day variable that cnt when queue is ended
                 graph[nx][ny] = graph[x][y] + 1
                 q.append((nx, ny))
 
-
 bfs()
 
-for row in graph:
-    for elem in row:
-        if elem == 0:
-            print(-1)
-            exit(0)
-    res = max(res, max(row))
-print(res - 1)
+# when while is ended,
+tot = 0
+for g in graph:
+    if 0 in g:
+        print(-1)
+        exit(0)
+    else:
+        tot = max(tot, max(g))
+print(tot - 1)
+# graph doesnt have any 0 -> print cnt
+# graph have 0 -> print -1
