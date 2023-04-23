4f194baa22670f6bf16128e3acbb85eca3991544
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2023-04-23 15:41:12 +0900

    [Gold IV] Title: 저울, Time: 184 ms, Memory: 115504 KB -BaekjoonHub

--- 백준/Gold/10159. 저울/저울.py
+++ 백준/Gold/10159. 저울/저울.py
@@ -1,24 +1,23 @@
-N = int(input())
-M = int(input())
 
-arr = [[0] * (N + 1) for _ in range(N + 1)]
-for i in range(M):
+n = int(input())
+m = int(input())
+
+graph = [[0] * (n + 1) for _ in range(n + 1)]
+
+for _ in range(m):
     a, b = map(int, input().split())
-    arr[a][b] = 1
-    arr[b][a] = -1
+    graph[a][b] = 1
 
-for k in range(1, N + 1):
-    for i in range(1, N + 1):
-        for j in range(1, N + 1):
-            if arr[i][k] == arr[k][j] and arr[i][k] != 0:
-                arr[i][j] = arr[i][k]
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if graph[i][k] and graph[k][j]:
+                graph[i][j] = 1
 
-for i in range(1, N + 1):
+for i in range(1, n + 1):
     cnt = 0
-    for j in range(1, N + 1):
-        if arr[i][j] == 0:
+    for j in range(1, n + 1):
+        if not graph[i][j] and not graph[j][i]:
             cnt += 1
-    print(cnt - 1)
-
     
-  
+    print(cnt - 1)
