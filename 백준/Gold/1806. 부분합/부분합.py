a33425031e36608b7a9ebf0df73b786a28020804
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2023-05-22 18:01:33 +0900

    [Gold IV] Title: 부분합, Time: 144 ms, Memory: 125300 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,20 +1,24 @@
+
 n, s = map(int, input().split())
-nums = list(map(int, input().split()))
-prefix = nums[0]
+num_list = list(map(int, input().split()))
+
+i = 0
+j = 0
 ans = 100001
-start = 0
-end = 0
+prefix = num_list[0]
 
 while True:
     if prefix >= s:
-        prefix -= nums[start]
-        ans = min(ans, end - start + 1)
-        start += 1
+        ans = min(ans, j - i + 1)
+        prefix -= num_list[i]
+        i += 1
     else:
-        end += 1
-        if end == n:
+        j += 1
+
+        if j == n:
             break
-        prefix += nums[end]
+
+        prefix += num_list[j]
 
 if ans == 100001:
     print(0)
