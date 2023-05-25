98aa3282a9ab1cea10e24e9960fb80d6a4a9c6e2
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2023-05-25 17:12:43 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 492 ms, Memory: 134236 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n, tc = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+# 누적합 그래프
+sum_arr = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        sum_arr[i][j] = arr[i - 1][j - 1] + sum_arr[i - 1][j] + sum_arr[i][j - 1] - sum_arr[i - 1][j -1]
+
+# 구간 더하기
+# 입력 좌표에서 -1 해주는 것 유의
+for _ in range(tc):
+    x1, y1, x2, y2 = map(int, input().split())
+    result = sum_arr[x2][y2] - sum_arr[x2][y1 - 1] - sum_arr[x1 - 1][y2] + sum_arr[x1 - 1][y1 - 1]
+    print(result)
