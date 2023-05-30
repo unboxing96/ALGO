2d9223d3469b429806802c7e98f2eb37584f1ffd
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2023-05-30 16:12:34 +0900

    [Silver IV] Title: 수열 (Easy), Time: 272 ms, Memory: 197420 KB -BaekjoonHub

---
+++ 백준/Silver/23827. 수열 （Easy）/수열 （Easy）.py
@@ -0,0 +1,13 @@
+
+n = int(input())
+nums = list(map(int, input().split()))
+
+prefix = 0
+ans = 0
+
+# 누적합 그래프 만들기
+for i in range(n-1):
+    prefix += nums[i]
+    ans += nums[i + 1] * prefix
+
+print(ans % 1000000007)
