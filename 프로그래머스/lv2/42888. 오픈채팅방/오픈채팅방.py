6dbfeff6c2ccd77bb58bcd24bffb2559dfa3d50f
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2023-06-20 00:45:28 +0900

    [level 2] Title: 오픈채팅방, Time: 91.39 ms, Memory: 47.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,27 +1,25 @@
+from collections import defaultdict
 
 def solution(record):
+    answer = []
 
     msg = []
-    dic = {}
+    dic = defaultdict(str)
 
     for rec in record:
-        # r= io, id, nick
-        r = list(rec.split())
 
-        if len(r) == 3:
-            if r[0] == "Enter":
-                dic[r[1]] = r[2]
-                msg.append((r[0], r[1]))
-            elif r[0] == "Change":
-                dic[r[1]] = r[2]
-        else:  # io == "Leave"
-            msg.append((r[0], r[1]))
+        order = list(rec.split())
 
-    result = []
+        if order[0] == "Enter" or order[0] == "Leave":
+            msg.append([order[0], order[1]])
+        
+        if order[0] == "Enter" or order[0] == "Change":
+            dic[order[1]] = order[2]
+
     for m in msg:
         if m[0] == "Enter":
-            result.append(f"{dic[m[1]]}님이 들어왔습니다.")
+            answer.append(f"{dic[m[1]]}님이 들어왔습니다.")
         else:
-            result.append(f"{dic[m[1]]}님이 나갔습니다.")
+            answer.append(f"{dic[m[1]]}님이 나갔습니다.")
 
-    return result
+    return answer
