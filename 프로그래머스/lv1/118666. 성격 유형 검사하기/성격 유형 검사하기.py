36cd47b0c63cc5ca7c6b88836b02ca17c4b591e5
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2023-06-30 17:40:56 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.12 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,42 +1,17 @@
 def solution(survey, choices):
+    answer = ''
+    dic= {"R" : 0,"T" : 0,"C" : 0,"F" : 0,"J" : 0,"M" : 0,"A" : 0,"N" : 0 }
     
-    dic = {
-    "R" : 0,
-    "T" : 0,
-    "C" : 0,
-    "F" : 0,
-    "J" : 0,
-    "M" : 0,
-    "A" : 0,
-    "N" : 0,}
-
-    # choices 개수만큼 반복하며, 딕셔너리에 기록
-    for i in range(len(choices)):
-        # 응답이 3보다 크면, 뒤의 성격 유형에 -3 뺀 만큼 점수 더하기
-        if choices[i] > 4:
-            dic[survey[i][1]] += (choices[i] - 4)
-        # 응답이 3보다 작으면, 앞의 성격 유형에 abs(4 - 값) 점수 더하기
-        elif choices[i] < 4:
-            dic[survey[i][0]] += abs(4 - choices[i])
-
-    result = []
-
-    # 성격 유형 고르기 함수
-    def pick(a, b):
-
-        if dic[a] != dic[b]:
-            result.append(max(a, b, key= lambda x : dic[x]))
-        elif dic[a] == dic[b]:
-            result.append(chr(min(ord(a), ord(b))))
-
-    # 4번 반복하며 성격 유형 고르기
-    pick("R", "T")
-    pick("C", "F")
-    pick("J", "M")
-    pick("A", "N")
+    for s,c in zip(survey,choices):
+        if c>4:     dic[s[1]] += c-4
+        elif c<4:   dic[s[0]] += 4-c
     
-    answer = ""
-    for i in result:
-        answer += i
+    li = list(dic.items())
+
+    for i in range(0, len(li), 2):
+        if li[i][1] < li[i+1][1]:
+            answer += li[i+1][0]
+        else:
+            answer += li[i][0]
     
     return answer
