ea85df9dfaf67243392cf3dd0e784ac12b50c60f
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2023-08-15 17:32:13 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,20 +1,10 @@
 def solution(lottos, win_nums):
-    
-    correct = []
-
-    for i in lottos:
-        if i in win_nums:
-            correct.append(i)
-
-    good = len(correct) + lottos.count(0)
-    bad = len(correct)
-
     answer = []
 
-    for score in [good, bad]:
-        if score < 2:
-            answer.append(6)
-        else:
-            answer.append(7 - score)
-            
-    return answer
+    base = set(lottos) & set(win_nums)
+    cnt_zero = lottos.count(0)
+
+    upper_rank = min(6, 7 - (len(base) + cnt_zero))
+    lower_rank = min(6, 7 - len(base))
+
+    return [upper_rank, lower_rank]
