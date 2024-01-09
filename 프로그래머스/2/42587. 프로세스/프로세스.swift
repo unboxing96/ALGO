8aba7c91da6f4301ab0fe451599bc9164b1c1614
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-01-09 18:14:15 +0900

    [level 2] Title: 프로세스, Time: 0.12 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.swift
+++ 프로그래머스/2/42587. 프로세스/프로세스.swift
@@ -1,39 +1,41 @@
-
-// 문제는 location에 위치한 값이 dequeue 되는 순간을 어떻게 측정할 것인가 ?
-// 쉽다 cnt 변수를 두면 된다
-// dequeue 할 때마다 cnt를 하나씩 올린다
-// location 위치만 true이고, priorities와 길이가 같은 Bool 배열을 만들어서, priorities 배열과 똑같이 queue를 돌리고, dequeue 될 때 해당 위치가 true인 경우에 cnt를 출력하도록 한다.
-
-// priorities를 순차적으로 탐색하며, dequeue한 값이 max 값과 일치하면 버린다. cnt += 1
-// 일치하지 않으면(더 작으면) 다시 enqueue 한다
-// 이것을 Bool 배열에도 똑같이 적용한다
-// true인 index가 버려지면 cnt를 return 한다
+// location은 targetIndex로 관리한다.
+// cPriorities.count > 0인 동안 반복한다.
+// priorities를 탐색하며, front 값보다 더 큰 값이 남아있는지 확인한다
+// 더 큰 값이 남아있다면, removeFirst -> append 한다
+    // 이때 targetIndex -= 1을 한다. 0보다 작아지면 배열의 크기를 값으로 한다.
+// 더 큰 값이 남아있지 않다면, removeFirst 한다
+    // 이때 targetIndex가 0이라면, 조건 달성이다. 전체 배열 - 현재 배열 + 1을 return 한다
+    // 아니라면 targetIndex -= 1을 한다.
 
 
 import Foundation
 
 func solution(_ priorities:[Int], _ location:Int) -> Int {
     
-    var priorities = priorities
-    var location = location
-    var result = 0
+    // location은 targetIndex로 관리한다.
+    var targetIndex = location
+    var cPriorities = priorities
     
-    while priorities.count > 0 {
-        location -= 1
-        let maxVal = priorities.max()!
-        let front = priorities[0]
-        if front != maxVal {
-            priorities.append(front)
-            priorities.removeFirst()
-            if location < 0 {
-                location = priorities.count - 1
-            }
+    // cPriorities.count > 0인 동안 반복한다.
+    while cPriorities.count > 0 {
+        // priorities를 탐색하며, front 값보다 더 큰 값이 남아있는지 확인한다
+        if cPriorities.contains(where: { $0 > cPriorities[0] }) {
+            // 더 큰 값이 남아있다면, removeFirst -> append 한다
+            let first = cPriorities.removeFirst()
+            cPriorities.append(first)
+            // 이때 targetIndex -= 1을 한다. 0보다 작아지면 배열의 크기를 값으로 한다.
+            targetIndex = targetIndex - 1 < 0 ? cPriorities.count - 1 : targetIndex - 1
         } else {
-            result += 1
-            priorities.removeFirst()
-            if location < 0 { break }
+            // 더 큰 값이 남아있지 않다면,
+            // 이때 targetIndex가 0이라면, 조건 달성이다. 전체 배열 - 현재 배열 + 1을 return 한다
+            if targetIndex == 0 {
+                return priorities.count - cPriorities.count + 1
+            }
+            // removeFirst 한다
+            // 아니라면 targetIndex -= 1을 한다.       
+            cPriorities.removeFirst()
+            targetIndex -= 1
         }
     }
-    
-    return result
+    return 0
 }
