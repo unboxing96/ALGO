76aadf3400d058eea990d2de2e3abab10befece0
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-02-01 15:52:27 +0900

    [level 3] Title: 네트워크, Time: 0.23 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+func find(_ x: Int, _ rootArr: inout [Int]) -> Int {
+    if rootArr[x] != x {
+        rootArr[x] = find(rootArr[x], &rootArr)
+    }
+    return rootArr[x]
+}
+
+func union(_ x: Int, _ y: Int, _ rootArr: inout [Int]) {
+    let rootX = find(x, &rootArr)
+    let rootY = find(y, &rootArr)
+    if rootX != rootY {
+        rootArr[rootY] = rootX
+    }
+}
+
+func solution(_ n: Int, _ computers: [[Int]]) -> Int {
+    var rootArr = Array(0..<n)
+
+    for i in 0..<n {
+        for j in 0..<n {
+            if computers[i][j] == 1 && i != j {
+                union(i, j, &rootArr)
+            }
+        }
+    }
+
+    let uniqueNetworks = Set(rootArr.map { find($0, &rootArr) })
+    return uniqueNetworks.count
+}
