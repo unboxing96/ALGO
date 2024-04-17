cb742bccb82d499a5785c19ffcdd19d318c8f9c7
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-04-17 11:36:12 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.27 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,62 +1,50 @@
 # 이해
-# 최소 거리 문제 -> BFS
-# 도착할 수 없을 경우 -> -1
-# 정사각형 형태 아님
+# 최단 경로 BFS
+# 도착할 수 없는 경우 -1 return
 
 # 풀이
-# maps의 0,0 위치에서 bfs()
-# if maps[n-1][m-1] != 1: 값 return else -1 return
-
-# bfs()
-# 큐 생성 후 첫 번째 노드 추가
-# 큐 반복
-    # x == n-1 and y == m-1이라면
-        # return maps[n-1][m-1]
-    # 4방향을 탐색하며, 범위를 벗어나지 않고, 방문하지 않고, 1인 경우
-        # 이전 값을 다음 위치에 추가 (방문 처리와 같다.)
-        # 큐에 추가
+# BFS로 풀이한다.
+# 누적 거리에 대한 정보를 maps에 그대로 덮어써도 무방하겠다.
+# 더 이상 이동할 곳이 없는 경우 즉시 -1을 return 한다.
+# n - 1, m - 1 위치에 도달한 경우 해당 위치의 값을 return 한다.
 
 from collections import deque
-        
+
 def solution(maps):
+    visited = [[False] * len(maps[0]) for _ in range(len(maps))]
+    return bfs(maps, visited)
+
+def bfs(maps, visited):
     n = len(maps)
     m = len(maps[0])
-    dx = [1, -1, 0, 0]
-    dy = [0, 0, 1, -1]
-    
-    answer = bfs(0, 0, n, m, dx, dy, maps)
-    return -1 if answer == 1 else answer
-
-def bfs(x, y, n, m, dx, dy, maps):
-    q = deque([(x, y)])
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    q = deque([(0, 0)])
+    visited[0][0] = True
     
     while q:
-        (x, y) = q.popleft()
+        x, y = q.popleft()
         
         if x == n - 1 and y == m - 1:
-            return maps[n-1][m-1]
+            return maps[x][y]
         
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             
-            if nx < 0 or nx >= n or ny < 0 or ny >= m: # 범위 벗어남
-                continue
-                
-            if maps[nx][ny] == 0: # 벽돌
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
             
-            if maps[nx][ny] > 1: # 방문했던 곳
-                continue
-            
-            maps[nx][ny] = maps[x][y] + 1 # 방문 처리, 거리 계산
-            q.append((nx, ny))
+            if not visited[nx][ny] and maps[nx][ny] != 0:
+                maps[nx][ny] = maps[x][y] + 1
+                visited[nx][ny] = True
+                q.append((nx, ny))
     
-    return 1
-    
-    
-    
-    
-    
-    
-    
+    return -1
+        
+        
+        
+        
+        
+        
+        
