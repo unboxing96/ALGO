1d66bc3715d30581a21bdf4131b5138c9bad3b18
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-04-17 10:56:53 +0900

    [level 3] Title: 네트워크, Time: 0.20 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,56 +1,44 @@
-# 풀이: BFS
-# 연결 요소를 탐색하기 위해서는 DFS/BFS 모두 좋다.
-# 파이썬에서 큐는 deque으로 구현한다.
-# 각 컴퓨터에 대한 visited 배열을 생성한다.
-# 인접 행렬을 탐색한다. 방문하지 않은 행(노드)을 만나면 bfs를 진행한다.
-    # bfs()
-        # 현재 노드를 방문 처리한다.
-        # 큐를 deque()으로 초기화하고, 현재 노드를 추가한다.
-        # 큐가 빌 때까지 반복한다.
-        # 큐에서 pop한다.
-        # pop한 노드의 인접 노드를 아직 방문하지 않았다면, 큐에 추가한다. 방문 처리한다.
-    # bfs가 종료되면 cnt += 1한다.
-# cnt를 return 한다.
+# 이해
+# 네트워크(연결된 덩어리)의 개수를 return
 
-from collections import deque
+# 풀이
+# 각 노드에 대한 visited 배열 생성
+# computers 탐색
+# 현재 노드를 방문하지 않았다면, dfs 진행
+# 탐색이 종료되면 cnt += 1
+# cnt를 return
 
 def solution(n, computers):
-
-    # 각 컴퓨터에 대한 visited 배열을 생성한다.
-    visited = [False] * n
-    cnt = 0
+    answer = 0
+    visited = [False] * len(computers)
     
-    # bfs()
-    def bfs(start):
-        # 현재 노드를 방문 처리한다.
-        # 큐를 deque()으로 초기화하고, 현재 노드를 추가한다.
-        visited[start] = True
-        queue = deque([start])
-        
-        # 큐가 빌 때까지 반복한다.
-        while queue:
-            # 큐에서 pop한다. 방문 처리한다.
-            v = queue.popleft()
-            
-            for i in range(n):
-                # pop한 노드의 인접 노드를 아직 방문하지 않았고 연결되었다면, 큐에 추가한다. 
-                if not visited[i] and computers[v][i] == 1:
-                    queue.append(i)
-                    visited[i] = True
-    
-    # 인접 행렬을 탐색한다. 방문하지 않은 행(노드)을 만나면 bfs를 진행한다.
-    for i in range(n):
+    for i in range(len(computers)):
         if not visited[i]:
-            bfs(i)
-            # bfs가 종료되면 cnt += 1한다.
-            cnt += 1
+            print("GO DFS")
+            answer += dfs(i, computers, visited)
     
-    return cnt
+    return answer
 
-
-
-
-
-
-
-
+def dfs(n, computers, visited):
+    print(f"Node: {n}")
+    visited[n] = True
+    
+    for idx, next in enumerate(computers[n]):
+        if not visited[idx] and next == 1:
+            dfs(idx, computers, visited)
+    
+    return 1
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
