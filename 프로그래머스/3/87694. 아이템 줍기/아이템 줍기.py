bad8448e56738461fe43498407d69f6e2f8d8345
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-04-17 15:33:28 +0900

    [level 3] Title: 아이템 줍기, Time: 0.22 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -1,80 +1,75 @@
-# 14:15
-
 # 이해
-# 복잡한 도형의 둘레를 따라서만 이동이 가능하다.
-# 시작 좌표와, 도착 좌표가 주어진다. 도착할 때까지 최소 거리는? BFS.
-# 좌표 개념이 배열과 반대이다. 현실 세계의 그것과 같다.
+# 여러 개의 다각형이 겹쳐져 있음
+# 겹쳐진 지형의 테두리를 따라서만 이동이 가능
+# 이때 특정 위치에서 특정 위치로 이동하는 최단 거리를 return
+
+# 주의할 점
+# 다각형이 겹치기 때문에, 의도치 않게 내부의 경로를 따라 움직일 가능성이 있다. -> 테두리는 1, 도형 안쪽은 -1로 채운다.
+# 두 개의 변이 딱 붙어있다면, 의도치 않게 테두리를 따라 이동하지 않고, 가로지를 가능성이 있다. -> 그래프를 2배로 확장
 
 # 풀이
-# 51 x 51 행렬을 만들고 0으로 초기화 한다.
-# rectangle을 탐색하면서, range(rec[0], rec[2] + 1): range(rec[1], rec[3] + 1): 범위의 테두리를 1로 만든다.
-# BFS 탐색을 한다.
-    # 큐를 생성하고 시작 노드를 큐에 넣는다.
-    # 큐를 반복한다.
-    # 원소를 pop한다.
-    # pop한 원소가 itemX, itemY와 같다면, return 한다.
-    # 4방향 탐색을 한다.
-    # nx, ny를 구한다.
-    # 범위를 벗어나면 continue 한다.
-    # 0을 만나면 continue 한다.
-    # 1보다 큰 값을 만나면 continue 한다.
-    # nx, ny 위치에서 4방향 탐색을 한번 더 한다.
-        # 여기서 4방향 중 하나라도 0을 만나야 한다. (그래야 테두리)
-        # 못 만나면 바깥 루프 기준 conitnue
-    # [nx][ny] = [x][y] + 1
-    # 큐에 (nx, ny) 추가
+# 최단 거리 -> BFS
+# 51 * 2 크기의 그래프를 생성한다.
+# rectangle 배열을 탐색하여 그래프를 채운다.
+    # 주어진 좌표를 모두 2를 곱한다.
+    # 테두리인 경우 1로 채운다.
+    # 테두리가 아닌 경우 -1로 채운다.
+# BFS를 진행한다.
+    # characterX, characterY, itemX, itemY 모두 2배로 만든다.
+    # 큐를 생성한다.
+    # 큐에 cX, cY를 삽입한다.
+    # 큐가 빌 때까지 반복한다.
+        # 큐에서 노드를 꺼낸다.
+        # 해당 노드가 iX, iY와 일치한다면 최단 거리를 return 한다.
+    # 범위를 벗어나면 continue
+    # nx, ny가 0 혹은 -1 이라면 continue
+    # nx, ny가 1인 경우에만 진행
+    # 방문 처리는 따로 필요 없지 않을까? 왜냐하면 nx, ny가 1인 경우 == 방문하지 않은 곳이기 때문에
 
 from collections import deque
 
 def solution(rectangle, characterX, characterY, itemX, itemY):
-    
-    cx, cy, ix, iy = characterX * 2, characterY * 2, itemX * 2, itemY * 2
-    graph = [[-1 for i in range(102)] for _ in range(102)]
-    visited = [[0 for i in range(102)] for _ in range(102)]
-    dx = [1, -1, 0, 0]
-    dy = [0, 0, 1, -1]
+    graph = [[0] * 102 for _ in range(102)]
     
     for rec in rectangle:
-        x1, y1, x2, y2 = map(lambda x: x*2, rec)
+        lx, ly, rx, ry = map(lambda x: x * 2, rec)
         
-        for i in range(x1, x2 + 1):
-            for j in range(y1, y2 + 1):
-                if x1 < i < x2 and y1 < j < y2:
-                    graph[i][j] = 0
-                elif graph[i][j] != 0:
-                    graph[i][j] = 1 # 테두리는 1, 내부는 0, 외부는 -1
+        for x in range(lx, rx + 1):
+            for y in range(ly, ry + 1):
+                if x == lx or x == rx or y == ly or y == ry:
+                    if graph[x][y] != -1:
+                        graph[x][y] = 1
+                else:
+                    graph[x][y] = -1
     
-    
-    answer = bfs(cx, cy, ix, iy, graph, visited, dx, dy)
-    return answer
-    
-    
-def bfs(characterX, characterY, itemX, itemY, graph, visited, dx, dy):
-    
-    q = deque([(characterX, characterY)])
+    return bfs(characterX, characterY, itemX, itemY, graph)
+
+
+def bfs(characterX, characterY, itemX, itemY, graph):
+    cx, cy, ix, iy = characterX * 2, characterY * 2, itemX * 2, itemY * 2
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    q = deque([(cx, cy)])
     
     while q:
-        (x, y) = q.popleft()
+        x, y = q.popleft()
         
-        if x == itemX and y == itemY:
-            return visited[itemX][itemY] // 2
+        if x == ix and y == iy:
+            return graph[x][y] // 2
         
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             
-            if nx < 1 or nx >= 102 or ny < 1 or ny >= 102: # 범위를 벗어남.
-                continue
-            
-            if visited[nx][ny]:
+            if nx < 0 or nx >= 102 or ny <= 0 or ny >= 102:
                 continue
             
             if graph[nx][ny] == 1:
-                visited[nx][ny] = visited[x][y] + 1
+                graph[nx][ny] = graph[x][y] + 1
                 q.append((nx, ny))
-    
-    return -1
-                
+
+
+
 
 
 
