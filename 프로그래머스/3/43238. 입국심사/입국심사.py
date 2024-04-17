ffcf9aa03aaf404959ec0591e8e675cd67bcf9e4
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-04-17 21:05:05 +0900

    [level 3] Title: 입국심사, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,31 +1,51 @@
 # 이해
-# 모든 심사대 비어있음
-# 한 심사대 동시에 한 명만
+# 여러 개의 심사대에 심사관이 한 명씩 있다
+# 각 심사관의 소요 시간은 다르다
+# 대기하는 사람은 비어있는 심사대에서 심사를 받을 수 있다
+# 혹은 기다렸다가 갈 수도 있다. 즉시 가지 않아도 된다는 뜻 ?
+# 모든 심사를 완료하는 최소 시간을 return
+# n은 심사를 기다리는 사람 수
+# times는 각 심사위원의 속도
 
 # 풀이
-# 피봇 포인터는 시간이다.
-# 포인터를 움직이며 n을 달성할 수 있는지를 기준으로 판단하자.
+# 이분탐색
+# 피봇의 범위와, 판단 기준을 선택해야 한다.
+# 피봇의 범위는 총 소요 시간으로 해야겠다.
+# 총 소요 시간을 times의 각 원소로 나누면, 해당 시간 내 심사관이 처리 가능한 인원 수가 나온다.
+# 이 인원 수를 판단 기준으로 한다.
+
+# Dr.code
+# left = 1
+# right = max(times) * n
+# while left <= right:
+    # mid = (left + right) // 2
+    # people = 0
+    # for time in times:
+        # people += mid // time
 
 def solution(n, times):
+    answer = int(1e9)
+    left = 1
+    right = max(times) * n
     
-    answer = 0
-    l = 1
-    r = max(times) * n
-    
-    while l <= r:
+    while left <= right:
+        mid = (left + right) // 2
         people = 0
-        mid = (l + r) // 2
-        
         for time in times:
             people += mid // time
-            
-            if people >= n:
-                break
-        
+    
         if people >= n:
             answer = mid
-            r = mid - 1
-        else:
-            l = mid + 1
-            
+            right = mid - 1
+
+        else: # people < n:
+            left = mid + 1
+        
     return answer
+
+
+
+
+
+
+
