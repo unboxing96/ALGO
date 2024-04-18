51e5d22d7e1774d6dee917901e4882ff29037323
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-04-18 20:08:40 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,12 +1,18 @@
-n = int(input())
-dp = [0] * (n + 1)
+# 이해
+# 피보나치 수열
+# 점화식은 dp[i] = dp[i - 1] + dp[i - 2]
+# dp[0] = 0
+# dp[1] = 1
+# dp[n]을 출력한다.
 
-if n < 2:
-    print(n)
-
-else:
+def solution(n):
+    dp = [0] * (n + 1)
     dp[1] = 1
-    dp[2] = 1
-    for i in range(3, n + 1):
-        dp[i] = max(dp[i], dp[i - 1] + dp[i - 2])
-    print(dp[n])
+
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    return dp[n]
+
+n = int(input())
+print(solution(n))
