a6acce77dab23408c9a12dedd32734506170b283
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-04-18 20:19:54 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,18 +1,17 @@
-# 이해
-# 피보나치 수열
-# 점화식은 dp[i] = dp[i - 1] + dp[i - 2]
-# dp[0] = 0
-# dp[1] = 1
-# dp[n]을 출력한다.
-
 def solution(n):
     dp = [0] * (n + 1)
-    dp[1] = 1
+    return fibo(dp, n)
 
-    for i in range(2, n + 1):
-        dp[i] = dp[i - 1] + dp[i - 2]
+def fibo(dp, x):
+    if x == 0 or x == 1:
+        return x
+    
+    if dp[x]:
+        return dp[x]
 
-    return dp[n]
+    dp[x] = fibo(dp, x-1) + fibo(dp, x-2)
+
+    return dp[x]
 
 n = int(input())
 print(solution(n))
