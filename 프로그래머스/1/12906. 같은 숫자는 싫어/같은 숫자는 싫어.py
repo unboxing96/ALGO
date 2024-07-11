127355491c1e161d0f3dcbef953f4cd9d967366c
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-07-11 13:52:59 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 61.90 ms, Memory: 27.9 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,18 +1,17 @@
-# 이해
-# 스택에 연속된 숫자가 저장되면 안 됨.
+# 문제 분석
+# 배열의 순서를 유지하며, 연속적으로 나타나는 숫자는 하나만 남기고 전부 제거
 
-# 풀이
-# arr을 탐색하며 stack에 추가
-# 추가하기 전, stack의 top과 같은 경우 pass, 다른 경우 push
+# 접근
+# 스택
+# 스택의 top이 현재 원소와 같으면 추가하지 않는다.
+# 나머지의 경우에는 추가한다.
+# 스택을 정순으로 출력한다.
 
 def solution(arr):
-    stack = []
+    answer = []
     
     for elem in arr:
-        if stack:
-            if elem != stack[-1]:
-                stack.append(elem)
-        else:
-            stack.append(elem)
-    
-    return stack
+        if not answer or answer[-1] != elem:
+            answer.append(elem)
+
+    return answer
