9690069e5d78594b0a3bd849f01b50ed97af9016
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-07-11 13:25:19 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,38 +1,40 @@
-# 이해
-# 1. 해당 장르의 재생 수
-# 2. 동일 장르 내 곡의 재생수
-# 3. 고유번호가 낮은 것
+# 문제 분석
+# 장르(genres)별로 각 음악의 번호(인덱스)와 재생 횟수(plays)가 주어짐.
+# 장르별로 재생 횟수가 높은 상위 2개까지 음악의 고유 번호(인덱스)를 배열에 추가한다.
+    # 해당 장르에 음악이 1개면, 1개만 한다.
+    # 재생 횟수가 동일하면, 음악의 고유 번호가 낮은 것을 먼저 수록한다.
 
-# 풀이
-# genre_count = { 장르: 총 재생 횟수 }
-# genre_dic = { 장르: [고유번호, 재생 횟수] }
-# genre_count -> 재생 횟수 -> 고유 번호 순서대로 정렬
-# 각 장르마다 2개씩 끊어서 고유 번호 저장
-
+# 접근
+# genres / plays 배열을 탐색하여, genresRanking 딕셔너리에 값을 더한다.
+    # 동시에 genresWithIndexAndPlayDict에 [[index, play]] 형태로 저장한다.
+# genresRanking의 value의 내림차순으로 genre를 얻는다. -> sortedGenres
+    # 해당 genre를 key로 gWIAPDict의 value를 얻는다.
+    # 해당 value를 play인 x[1]을 내림차순, index인 x[0]을 오름차순으로 정렬한다. 
+    # 정렬한 배열을 [:2]까지 index 만을 answer에 추가한다.
+# answer를 return한다.
+    
+    
 def solution(genres, plays):
     answer = []
+    genresRanking = {}
+    genresWithIndexAndPlayDict = {}
     
-    genre_count = {}
-    genre_dic = {}
-    
-    # 장르와 재생 횟수를 저장
     for i in range(len(genres)):
-        if genres[i] in genre_count:
-            genre_count[genres[i]] += plays[i]
+        if genresRanking.get(genres[i], 0):
+            genresRanking[genres[i]] += plays[i]
+            genresWithIndexAndPlayDict[genres[i]].append([i, plays[i]])
         else:
-            genre_count[genres[i]] = plays[i]
-        
-        genre_dic.setdefault(genres[i], []).append([i, plays[i]])
+            genresRanking[genres[i]] = plays[i]
+            genresWithIndexAndPlayDict[genres[i]] = []
+            genresWithIndexAndPlayDict[genres[i]].append([i, plays[i]])
     
-    # 장르를 재생 횟수에 따라 내림차순 정렬
-    genre_count_sorted = sorted(genre_count.items(), key=lambda x: x[1], reverse=True)
+    sortedGenres = [item[0] for item in sorted(genresRanking.items(), key=lambda x: -x[1])]
+    print(genresRanking)
+    print(genresWithIndexAndPlayDict)
     
-    for genre, _ in genre_count_sorted:
-        # 각 장르 내의 곡을 먼저 재생 횟수, 그 다음 고유 번호에 따라 정렬
-        sorted_songs = sorted(genre_dic[genre], key=lambda x: (-x[1], x[0]))
-        # 각 장르에서 상위 2곡의 고유 번호를 추출하여 추가
-        answer.extend([song[0] for song in sorted_songs[:2]])
-    
+    for genre in sortedGenres:
+        dict = genresWithIndexAndPlayDict[genre]
+        sortedDict = sorted(dict, key = lambda x : (-x[1], x[0]))
+        answer.extend([item[0] for item in sortedDict][:2])
+
     return answer
-
-
