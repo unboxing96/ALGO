a0195a818295da0c32494c424cb94fa218ede748
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-07-11 09:34:52 +0900

    [level 1] Title: 폰켓몬, Time: 0.21 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,29 +1,15 @@
-# 이해
-# N마리 중에서 N/2마리를 선택.
-# 이때 가장 많은 종류를 선택하고, 그때의 가짓수를 return
-# nums의 길이는 10,000이므로 O(n^2)까지는 괜찮겠다. 프로그래머스는 10억에서 시간 초과가 발생하므로.
+# 문제 분석
+# N마리 포켓몬 중에서 N/2마리를 선택한다.
+# 이때 선택한 포켓몬의 가짓수가 최대가 되도록 하라.
 
-# 풀이
-# nums+1(0부터 200,000) 크기의 visited 배열을 만든다.
-# nums를 탐색하며, 방문하지 않은 포켓몬일 경우 result, cnt에 += 1 하고, 방문 처리한다.
-# 매 탐색마다 cnt가 len(nums) // 2 이하인지 확인한다. 초과하면 result를 return 한다.
+# 접근
+# N마리 포켓몬의 종류가 모두 다른 경우, 최대값은 N/2이다.
+# N마리 포켓몬의 종류가 모두 같은 경우, 최대값은 1이다.
+# 배열의 길이를 n, 원소의 가짓수를 k라고 하자
+    # n/2과 k 중에 작은 값을 return 하면 되겠다.
+
 
 def solution(nums):
-    cnt = 0
-    size = len(nums)
-    visited = [False] * (size + 1)
-    variety = len(set(nums))
-    
-    if variety <= size // 2:
-        return variety
-    
-    for i, num in enumerate(nums):
-        if not visited[i]:
-            visited[i] = True
-            
-            if cnt + 1 <= size // 2:
-                cnt += 1
-            else:
-                break
-    
-    return cnt
+    n = len(nums)
+    k = len(set(nums))
+    return min(n/2, k)
