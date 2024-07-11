da459b959ce3a3407fcb5766c1f8021e428ebaef
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-07-11 22:41:05 +0900

    [level 2] Title: 주식가격, Time: 16.81 ms, Memory: 17.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,27 +1,24 @@
-# 이해
-# 배열의 각 원소가 오름차순이 지속되는 길이를 각각 return
-# 다음 원소와 비교하여, 값이 작아지지 않을 때마다 += 1
-# 일단 지속 시간을 1초 증가시키고, 그 뒤에 오름차순 여부를 판단한다.
-# 마지막 원소는 0초이다.
+# 문제 분석
+# 탑 보기, 올영 코테와 비슷한 문제
+# 차이점은 건물 뒤편까지 판단해야 한다는 것
 
-# 풀이
-# 일단 이중 반복문으로 풀자
-
-from collections import deque
+# 접근
+# answer를 초기화 한다. 배열의 각 요소가 얻을 수 있는 최대값으로 한다.
+# stack을 초기화 한다.
+# prices 배열을 탐색한다. (1, n)
+    # while stack and prices[stack[-1]] > prices[i]:
+        # j = stack.pop()
+        # answer[i] = i - j
+    # stack.append(i)
 
 def solution(prices):
-    answer = []
-    queue = deque(prices)
+    answer = [i for i in range(len(prices) - 1, -1, -1)]
+    stack = [0]
     
-    while queue:
-        price = queue.popleft()
-        sec = 0
-        
-        for q in queue:
-            sec += 1
-            if price > q:
-                break
-        
-        answer.append(sec)
-    
+    for i in range(1, len(prices)):
+        while stack and prices[stack[-1]] > prices[i]:
+            j = stack.pop()
+            answer[j] = i - j
+        stack.append(i)
+
     return answer
