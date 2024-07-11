90a3e0b594cd3e291b96a1a30a15b6c261776415
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-07-11 15:07:08 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,32 +1,37 @@
-# 이해
-# progresses의 원소는 100%를 달성하면 배포된다.
-# 매일 speeds의 원소만큼 progress가 진행된다.
-# 가장 앞에 있는 원소가 100%를 달성한 순간에, 100%를 달성한 모든 원소를 배포한다.
-
-# 풀이
-# progresses의 각 원소를 100에서 빼고, 그 값을 speeds로 나누어 남은 일자를 계산한다. remain_days: [Int]
-# remain_days를 탐색하며, 내림차순으로 정렬된 경우 tmp에 더하고, 
-    # 내림차순이 깨진 순간 tmp를 answer에 append하고, tmp를 초기화 한다.
-# answer를 return한다.
-
-import math
+# 문제 분석
+# progresses는 현재까지 진도를 담고 있다. 인덱스가 곧 배포 순서이다.
+# speeds는 개발 속도이다.
+# 매일 speed만큼 진도를 나간다.(progresses 배열의 원소에 더한다)
+# progress가 100이 되면 배포 준비를 마친다.
+# 가장 앞에 있는 progress가 배포 준비를 마치면 즉시 배포된다.
+    # 이때, 앞에서부터 순서대로 배포 준비를 마친 것들이 모두 배포된다.
+    
+# 접근
+# progresses와 speeds를 동시에 탐색하며, 배포까지 남은 일수를 구한다. (leftovers)
+# leftovers를 뒤집는다. 스택 연산으로 게산하기 편하게.
+# top 요소를 제거하면서, 해당 값 이하의 요소도 모두 제거한다.
+    # 제거하는 수만큼 count해서 answer에 추가한다.
+# answer를 return 한다.
 
 def solution(progresses, speeds):
-    days_needed = [math.ceil((100 - p) / s) for p, s in zip(progresses, speeds)]
-    result = []
+    answer = []
+    leftovers = []
     
-    current_max_day = days_needed[0]
-    count = 0
+    for i in range(len(progresses)):
+        quotient = (100 - progresses[i]) // speeds[i]
+        remainder = (100 - progresses[i]) % speeds[i]
+        if remainder:
+            quotient += 1
+        leftovers.append(quotient)
     
-    for day in days_needed:
-        if day <= current_max_day:
-            count += 1
-        else:
-            result.append(count)
-            current_max_day = day
-            count = 1
-            
-    result.append(count)  # 마지막 배치를 추가
-    return result
+    leftovers.reverse()
     
+    while leftovers:
+        top = leftovers.pop()
+        cnt = 1
+        while leftovers and leftovers[-1] <= top:
+            leftovers.pop()
+            cnt += 1
+        answer.append(cnt)
+        
     return answer
