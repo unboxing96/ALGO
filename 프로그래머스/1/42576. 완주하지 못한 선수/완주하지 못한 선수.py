0c4a5830a765cffbcc1035659c742e813051f901
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-07-11 09:53:28 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 42.89 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,45 +1,26 @@
-# 이해
-# completion에 있지만, participant에 없는 사람 return
-# 동명이인이 있을 수 있다.
-# 딕셔너리로는 애매할 듯?
-# set도 애매하겠다.
-# 선형 탐색으로 중복된 이름을 찾는다.
-# 딕셔너리로 체크한다.
-# 모든 값이 체크되면, 중복된 이름을 return 하고 아니라면, 체크되지 않은 이름을 return 한다.
+# 문제 분석
+# participant에 참가자 배열
+# completion에 완주자 배열. 참가자보다 1명 적다.
+# 미완주자를 return
+# 동명이인이 있다.
 
-# 정렬한다.
-# 순서대로 나오지 않으면 즉시 return 한다.
-
-# def solution(participant, completion):
-    
-#     dic = {}
-#     tmp = 0
-    
-#     for part in participant:
-#         dic[hash(part)] = part
-#         tmp += hash(part)
-    
-#     for comp in completion:
-#         tmp -= hash(comp)
-        
-#     return dic[tmp]
-
-from collections import defaultdict
+# 접근
+# participant 배열 요소의 개수를 세는 딕셔너리participantDict를 생성한다.
+# completion을 탐색하며 participantDict에서 대응하는 값을 뺀다.
+# 마지막에 값이 1인 유일한 원소의 key를 출력한다.
 
 def solution(participant, completion):
     
-    dic = defaultdict(int)
-    
+    participantDict = {}
     for part in participant:
-        dic[part] += 1
-        
+        if participantDict.get(part, 0):
+            participantDict[part] += 1
+        else:
+            participantDict[part] = 1
+    
     for comp in completion:
-        dic[comp] -= 1
+        participantDict[comp] -= 1
     
-    
-    answer = ""
-    for key, val in dic.items():
-        if val == 1:
-            answer = key
-    
-    return answer
+    for k, v in participantDict.items():
+        if v == 1:
+            return k
