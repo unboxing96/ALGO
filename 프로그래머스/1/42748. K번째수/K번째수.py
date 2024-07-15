9308d714d68a35d9d719913865b6b802daba0da7
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-07-15 09:37:42 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,15 +1,15 @@
-# 이해
-# 1. 슬라이싱
-# 2. 정렬
-# 3. 인덱스 return
+# 문제 분석
+# 1. 슬라이싱 i - 1 ..< j
+# 2. 오름차순 정렬
+# 3. k - 1 번째 숫자 return
 
 def solution(array, commands):
     answer = []
     
-    for command in commands:
-        i, j, k = command[0], command[1], command[2]
-        new_arr = array[i-1 : j]
-        new_arr.sort()
-        answer.append(new_arr[k - 1])
+    for com in commands:
+        i, j, k = com[0], com[1], com[2]
+        slicedArr = array[i - 1 : j]
+        sortedArr = sorted(slicedArr)
+        answer.append(sortedArr[k - 1])
     
     return answer
