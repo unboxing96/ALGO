926e3cd1ed126b4b330a2ac061c7d6734e1c5a47
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-07-16 14:33:26 +0900

    [level 2] Title: 카펫, Time: 0.14 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.01 ms
+메모리: 10.1 MB, 시간: 0.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 13일 12:57:54
+2024년 07월 16일 14:33:24
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,27 +1,21 @@
-# 이해
-# yellow, brown 값을 보고 2차원 배열의 형태를 [가로, 세로]로 return
+# 문제 분석
+# 노란색 격자와 갈색 격자의 개수를 입력 받았을 때, 배열의 가로, 세로를 return
+# 가로는 세로 길이 이상이다.
 
-# 풀이
-# brown은 언제나 (yellow_가로 + 2) * 2 + (yellow_세로) * 2 이다.
-# yellow의 형태를 (1, 24), (2, 12), (3, 8), (4, 6) 순서로 바꿔가며 brown이 입력값과 일치하는지 판단.
-
-# 의사코드
-# 가로 i: 1부터 yellow의 제곱근 보다 작은 정수의 범위까지 탐색
-# 세로: yellow // i
-# yellow의 가로와 세로 길이로 brown의 값을 구한 뒤에 일치할 때까지 진행.
-# 일치하면 [yellow_가로 + 2, yellow_세로 + 2] return
+# 접근
+# 갈색 격자의 개수는 노란색 격자와 상당히 상당한 상관이 있다.
+# 노란 격자의 가로를 w, 세로를 h라고 하면, (w + 2) * (h * 2) <- 이것이 전체 개수이다.
+# 노란 격자 개수의 1/2 범위 내에 약수를 구하고, 위 공식 == 노란 + 갈색일 때가 정답이다.
 
 def solution(brown, yellow):
     
-    for i in range(1, int(yellow ** 0.5) + 1):
+    for i in range(1, yellow + 1):
         if yellow % i == 0:
-            y_w = i
-            y_h = yellow // i
-        
-        if brown == ((y_w + 2) * 2 + y_h * 2):
-            b_w = max(y_w, y_h) + 2
-            b_h = min(y_w, y_h) + 2        
-            return [b_w, b_h]
+            w = i
+            h = yellow // i
+            nw = w + 2
+            nh = h + 2
+            size = brown + yellow
 
-
-
+            if nw * nh == size:
+                return [max(nw, nh), min(nw, nh)]
