63b769e943a5ba4cb51038a15dec4e0efe24cad2
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-07-16 09:24:21 +0900

    [level 1] Title: 모의고사, Time: 2.13 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,49 +1,38 @@
-# 풀이
-# 각 수포자의 패턴을 반복하는 배열을 만듦. 10000개까지.
-# 가장 패턴의 길이가 짧은 수포자 1을 기준으로 하여, 2000번씩 반복함
-# 반복문으로 answers를 탐색하며, 각 수포자의 점수를 계산
-# 최고점을 맞은 수포자를 배열로 return
+# 문제 분석
+# 각 수포자는 일정한 패턴으로 답을 찍는다.
+# 정답 배열이 주어지면, 패턴과 답을 맞춰보며 점수를 기록한다
+# 최고점을 기록한 수포자의 인덱스를 return 한다.
+# 동점자가 있으면, 인덱스의 오름차순으로 return 한다.
 
-# 의사코드
-# one_array = [1, 2, 3, 4, 5] * 2000
-# two_array = [2, 1, 2, 3, 2, 4, 2, 5] * 2000
-# three_array = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] * 2000
-# one_score = 0
-# two_score = 0
-# three_score = 0
-# for i in range(len(answers)):
-# if one_array[i] == answers[i]:
-# one_score += 1
-# ...
-# top_score = max(one_score, ...)
-# score_array = [one_score, two_score, three_score]
-# for i in range(3):
-# if score_array[i] == top_score:
-# answer.append(i + 1)
+# 접근
+# 각 수포자 패턴에 대하여 길이가 10,000 이상인 배열을 생성한다.
+# 정답을 맞춰보아, 각 수포자의 점수를 기록한다.
+# 점수의 max 값을 구하고, 인덱스 순서대로 배열을 탐색하며, max와 점수와 같은 것을 배열에 삽입한다.
+# 배열을 return한다.
 
 def solution(answers):
     answer = []
     
-    one_array = [1, 2, 3, 4, 5] * 2000
-    two_array = [2, 1, 2, 3, 2, 4, 2, 5] * 2000
-    three_array = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] * 2000
-    one_score = 0
-    two_score = 0
-    three_score = 0
+    s1 = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5] * 1000
+    s2 = [2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5] * 1000
+    s3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5] * 1000
     
-    for i in range(len(answers)):
-        if one_array[i] == answers[i]:
-            one_score += 1
-        if two_array[i] == answers[i]:
-            two_score += 1
-        if three_array[i] == answers[i]:
-            three_score += 1
-
-    top_score = max(one_score, two_score, three_score)
-    score_array = [one_score, two_score, three_score]
+    scores = [0, 0, 0]
     
-    for i in range(3):
-        if score_array[i] == top_score:
-            answer.append(i + 1)
+    for idx, ans in enumerate(answers):
+        if s1[idx] == ans:
+            scores[0] += 1
+        
+        if s2[idx] == ans:
+            scores[1] += 1
+        
+        if s3[idx] == ans:
+            scores[2] += 1
+    
+    maxScore = max(scores)
+    
+    for idx, score in enumerate(scores):
+        if score == maxScore:
+            answer.append(idx + 1)
     
     return answer
