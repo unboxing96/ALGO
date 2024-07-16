505b5bc97284d9550153a316a4909558b344ec04
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-07-16 17:43:28 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 5.60 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,43 +1,40 @@
-# 이해
-# 연결 관계를 나타내는 wires가 주어짐
-# wires 중에 하나를 제거했을 때, 나누어진 두 전력망의 차이(절댓값)의 최솟값을 return
+# 접근
+# 모든 노드를 연결한다
+# 입력으로 주어진 wires를 탐색하며 각 연결을 삭제하고, 탐색하여 answer를 갱신하고, 다시 연결한다.
 
-# 풀이
-# wires의 원소를 하나씩 빼고 연결 그래프를 생성
-# 연결 그래프의 첫 번째 노드를 시작으로, 연결된 네트워크의 크기를 구함
-# 두 개 네트워크의 크기의 차이로 갱신
+# 탐색은?
+# 어차피 모든 노드를 탐색해야 해서 DFS, BFS의 차이는 없을 것 같다.
+# 매번 visited 배열을 초기화 하면서, 현재 노드를 방문하지 않았으면 dfs 혹은 bfs를 하면 되겠다.
+# dfs로 하자.
+
+
+def dfs(start, graph, visited, check_link):
+    cnt = 1
+    visited[start] = True
+    
+    for adj_node in graph[start]:
+        if not visited[adj_node] and check_link[start][adj_node]:
+            cnt += dfs(adj_node, graph, visited, check_link)
+    
+    return cnt
 
 def solution(n, wires):
-    diff = int(1e9)
+    answer = 10000000
     
-    def dfs(now, graph, visited):
-        visited[now] = True
-        cnt = 1
-        
-        for neighbor in graph[now]:
-            if not visited[neighbor]:
-                cnt += dfs(neighbor, graph, visited)
-        
-        return cnt
+    check_link = [[True] * (n+1) for _ in range(n+1)]
+    graph = [[] for _ in range(n+1)]
     
-    
-    graph = [[] for _ in range(n + 1)]
     for a, b in wires:
         graph[a].append(b)
         graph[b].append(a)
-
+    
     for a, b in wires:
-        graph[a].remove(b)
-        graph[b].remove(a)
-
-        visited = [False] * (n + 1)
-        size_one_network = dfs(a, graph, visited)
-        current_diff = abs((n - size_one_network) - size_one_network)
-        diff = min(diff, current_diff)
-
-        graph[a].append(b)
-        graph[b].append(a) # 그래프 원상복구
+        visited = [False] * (n+1)
+        check_link[a][b] = False
+        cnt_a = dfs(a, graph, visited, check_link)
+        cnt_b = dfs(b, graph, visited, check_link)
+        check_link[a][b] = True
         
-    return diff
-
-
+        answer = min(answer, abs(cnt_a - cnt_b))
+    
+    return answer
