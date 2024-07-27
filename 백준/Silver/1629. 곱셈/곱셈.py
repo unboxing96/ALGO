a67487bd135bed109210e693edcfe765055fafff
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-07-27 23:35:41 +0900

    [Silver I] Title: 곱셈, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -0,0 +1,15 @@
+a, b, c = map(int, input().split())
+
+def multiply(a, b):
+    if b == 1:
+        return a % c
+    
+    temp = multiply(a, b // 2)
+
+    if b % 2 == 0:
+        return temp * temp % c
+    else:
+        return (temp * temp % c) * a % c
+    
+result = multiply(a, b)
+print(result)
