a7a2af278ead68c906f9efd539dc26144c22d33b
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-07-30 22:42:33 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 115 ms, Memory: 45,300 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
@@ -1,3 +1,22 @@
+#1.
+
+# 문제 분석
+# 평탄화 한다.
+# 더 이상 평탄화 할 수 없으면, 입력값에 따라 0 혹은 1을 출력
+# 평탄화 횟수에는 제한이 있다.
+
+# 접근
+# 가로 길이가 100이므로 완전 탐색해도 풀이할 수 있다.
+# 카운트 배열을 생성한다.
+# 주어진 횟수만큼 반복한다.
+    # 아래에서 가장 큰 인덱스와 가장 작은 인덱스는 0이면 안 된다. 값이 있어야 한다.
+    # 가장 큰 값을 갖고 있는 인덱스 (끝 인덱스) -= 1
+    # 가장 작은 값을 갖고 있는 인덱스 -= 1
+    # 그 다음 작은 값을 갖고 있는 인덱스 += 1
+    
+# TestCase 개수
+T = 10
+
 def my_max(iterable):
     max_num = iterable[0]
     for i in range(1, len(iterable)):
@@ -6,7 +25,7 @@ def my_max(iterable):
     return max_num
 
 # TestCase만큼 반복
-for tc in range(1, 10 + 1):
+for tc in range(1, T + 1):
     n = int(input())
     arr = list(map(int, input().split()))
 
