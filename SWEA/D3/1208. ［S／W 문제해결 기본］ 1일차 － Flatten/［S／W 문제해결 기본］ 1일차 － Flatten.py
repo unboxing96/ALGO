46bcd315022b9721e0926a09bb73918d3daf77dc
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-07-30 14:09:45 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 115 ms, Memory: 45,300 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
@@ -1,9 +1,40 @@
-for t in range(1, 11):
-    dump = int(input())
-    height = list(map(int, input().split()))
+def my_max(iterable):
+    max_num = iterable[0]
+    for i in range(1, len(iterable)):
+        if max_num < iterable[i]:
+            max_num = iterable[i]
+    return max_num
 
-    for i in range(dump):
-        height[height.index(min(height))] += 1
-        height[height.index(max(height))] -= 1
+# TestCase만큼 반복
+for tc in range(1, 10 + 1):
+    n = int(input())
+    arr = list(map(int, input().split()))
 
-    print(f"#{t}", max(height) - min(height))
+    counts = [0] * (my_max(arr) + 1)
+    for i in range(len(arr)):
+        counts[arr[i]] += 1
+
+    s = 0
+    e = len(counts) - 1
+    cnt = 0
+    while s + 1 < e and cnt < n:
+        if not counts[e]:
+            e -= 1
+            continue
+        if not counts[s]:
+            s += 1
+            continue
+
+        counts[e] -= 1
+        counts[s] -= 1
+        counts[s + 1] += 1
+        counts[e - 1] += 1
+        cnt += 1
+
+    while counts[s] == 0: # 탐색 끝낸 뒤에, 가장 낮은 높이
+        s += 1
+
+    while counts[e] == 0: # 탐색 끝낸 뒤에, 가장 높은 높이
+        e -= 1
+
+    print(f'#{tc} {e - s}')
