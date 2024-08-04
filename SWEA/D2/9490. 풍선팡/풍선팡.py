192aa2081808c9e4a1dda414a935a55c6dff7c3d
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-08-05 08:44:09 +0900

    [D2] Title: 풍선팡, Time: 261 ms, Memory: 49,900 KB -BaekjoonHub

--- SWEA/D2/9490. 풍선팡/풍선팡.py
+++ SWEA/D2/9490. 풍선팡/풍선팡.py
@@ -1,32 +1,24 @@
-# 문제 분석
-# 풍선에 들어있는 개수만큼 상하좌우
-
 # TestCase 개수
 T = int(input())
 
 # TestCase만큼 반복
 for tc in range(1, T + 1):
-    N, M = map(int, input().split())
-    matrix = [list(map(int, input().split())) for _ in range(N)]
-    dx = [0, 1, -1, 0]  # 우, 하, 좌, 상
-    dy = [1, 0, 0, -1]
-    max_value = -1e9
+    n, m = map(int, input().split())
+    matrix = [list(map(int, input().split())) for _ in range(n)]
+    dx = [0, 0, 1, -1]
+    dy = [1, -1, 0, 0]
+    max_tmp = -1e9
 
-    for x in range(N):
-        for y in range(M):
+    for x in range(n):
+        for y in range(m):
             tmp = matrix[x][y]
-            poong = matrix[x][y]
-            for i in range(4):
-                for step in range(1, poong + 1):
+            step_range = matrix[x][y]
+            for step in range(1, step_range + 1):
+                for i in range(4):
                     nx = x + dx[i] * step
                     ny = y + dy[i] * step
+                    if 0 <= nx < n and 0 <= ny < m:
+                        tmp += matrix[nx][ny]
+            max_tmp = max(max_tmp, tmp)
 
-                    if nx < 0 or nx >= N or ny < 0 or ny >= M:
-                        continue
-
-                    tmp += matrix[nx][ny]
-
-            if max_value < tmp:
-                max_value = tmp
-
-    print(f'#{tc} {max_value}')
+    print(f'#{tc} {max_tmp}')
