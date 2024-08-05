b912af69a021b6724d9731446fbf2a07391e973a
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-08-05 23:09:36 +0900

    [Silver II] Title: 유기농 배추, Time: 180 ms, Memory: 280816 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,42 +1,35 @@
+import sys
+sys.setrecursionlimit(10 ** 5)
 
-from collections import deque
+def dfs(x, y, cnt):
+    matrix[x][y] = -1 # 방문 처리
 
-tc = int(input())
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
 
+        if 0 <= nx < n and 0 <= ny < m and matrix[nx][ny] == 1:
+            cnt = dfs(nx, ny, cnt + 1)
 
-def bfs(x, y):
-    graph[x][y] = 0
-    q = deque()
-    q.append((x, y))
+    return cnt
 
-    while q:
-        x, y = q.popleft()
+T = int(input())
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+for tc in range(T):
+    m, n, k = map(int, input().split())
+    matrix = [[0] * m for _ in range(n)]
 
-            if 0 <= nx < n and 0 <= ny < m:
-                if graph[nx][ny] == 1:
-                    graph[nx][ny] = 0
-                    q.append((nx, ny))
+    for _ in range(k):
+        b, a = map(int, input().split())
+        matrix[a][b] = 1
 
-
-for _ in range(tc):
-    n, m, e = map(int, input().split())
-    graph = [[0] * m for _ in range(n)]
-
-    for _ in range(e):
-        a, b = map(int, input().split())
-        graph[a][b] = 1
-
-    time = 0
+    result = 0
     for i in range(n):
         for j in range(m):
-            if graph[i][j] == 1:
-                bfs(i, j)
-                time += 1
+            if matrix[i][j] == 1:
+                if dfs(i, j, 1):
+                    result += 1
 
-    print(time)
+    print(result)
