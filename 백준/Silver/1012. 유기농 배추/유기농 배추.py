7379e7546f702914ec5c5cd04a0494c53332b212
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-08-05 23:12:33 +0900

    [Silver II] Title: 유기농 배추, Time: 148 ms, Memory: 114032 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,17 +1,25 @@
-import sys
-sys.setrecursionlimit(10 ** 5)
 
-def dfs(x, y, cnt):
-    matrix[x][y] = -1 # 방문 처리
+from collections import deque
 
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
+def bfs(a, b):
+    q = deque()
+    q.append((a, b))
+    cnt = 1
 
-        if 0 <= nx < n and 0 <= ny < m and matrix[nx][ny] == 1:
-            cnt = dfs(nx, ny, cnt + 1)
+    while q:
+        x, y = q.popleft()
 
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < m and matrix[nx][ny] == 1:
+                matrix[nx][ny] = -1
+                q.append((nx, ny))
+                cnt += 1
+    
     return cnt
+
 
 T = int(input())
 dx = [1, -1, 0, 0]
@@ -29,7 +37,9 @@ for tc in range(T):
     for i in range(n):
         for j in range(m):
             if matrix[i][j] == 1:
-                if dfs(i, j, 1):
+                # if dfs(i, j, 1):
+                #     result += 1
+                if bfs(i, j):
                     result += 1
 
     print(result)
