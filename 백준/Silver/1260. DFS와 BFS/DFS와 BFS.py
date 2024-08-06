a041eb8a176c473100e78224c899ae7ae079dcaf
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-08-06 23:03:54 +0900

    [Silver II] Title: DFS와 BFS, Time: 148 ms, Memory: 115648 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,42 +1,40 @@
-def dfs(now):
-    visited[now] = True
-    print(now, end = " ")
+from collections import deque
 
-    for next in matrix[now]:
-        if not visited[next]:
-            dfs(next)
-
-def bfs(now):
-    q = deque([now])
-    visited[now] = True
-
+def bfs(v):
+    q = deque()
+    q.append(v)
+    visited[v] = False
+    
     while q:
         x = q.popleft()
         print(x, end = " ")
+        
+        for next_node in matrix[x]:
+            if visited[next_node]:
+                visited[next_node] = False
+                q.append(next_node)
 
-        for next in matrix[x]:
-            if not visited[next]:
-                visited[next] = True
-                q.append(next)
+def dfs(v):
+    visited[v] = True
+    print(v, end = " ")
+    
+    for next_node in matrix[v]:
+        if not visited[next_node]:
+            dfs(next_node)
 
-
-from collections import deque
-
+    
 n, m, v = map(int, input().split())
 matrix = [[] for _ in range(n + 1)]
+visited = [False] * (n + 1)
 
-for i in range(m):
+for _ in range(m):
     a, b = map(int, input().split())
     matrix[a].append(b)
     matrix[b].append(a)
 
-for row in matrix:
-    row.sort()
-
-visited = [False] * (n + 1)
+for m in matrix:
+    m.sort()
+    
 dfs(v)
-
 print()
-
-visited = [False] * (n + 1)
 bfs(v)
