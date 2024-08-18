522056bdfeca520c8238903b412e8d932b3f96d5
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-08-18 17:37:06 +0900

    [D2] Title: 파리 퇴치, Time: 197 ms, Memory: 61,996 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -1,20 +1,18 @@
-# TestCase 개수
+
 T = int(input())
 
-# TestCase만큼 반복
 for tc in range(1, T + 1):
-    N, M = map(int, input().split())
-    matrix = [list(map(int, input().split())) for _ in range(N)]
-    max_tmp = -1e9
+    n, m = map(int, input().split())
+    matrix = [list(map(int, input().split())) for _ in range(n)]
+    max_tmp = 0
 
-    for i in range(N - (M - 1)):
-        for j in range(N - (M - 1)):
+    for i in range(n - m + 1):
+        for j in range(n - m + 1):
             tmp = 0
-            for ni in range(i, i + M):
-                for nj in range(j, j + M):
-                    tmp += matrix[ni][nj]
-
-            if max_tmp < tmp:
-                max_tmp = tmp
-
-    print(f'#{tc} {max_tmp}')
+            for mi in range(m):
+                for mj in range(m):
+                    tmp += matrix[i + mi][j + mj]
+            max_tmp = max(max_tmp, tmp)
+    
+    print(f"#{tc} {max_tmp}")
+            
