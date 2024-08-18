4603c811af58277aeeb10d617c1b671f5b3726b8
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-08-18 11:20:00 +0900

    [Bronze I] Title: 딱지놀이, Time: 180 ms, Memory: 112144 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/14696. 딱지놀이/딱지놀이.py
@@ -0,0 +1,25 @@
+n = int(input())
+for _ in range(n):
+    left = []
+    right = []
+    for i in range(2):
+        cnt, *arr = map(int, input().split())
+        if i == 0:
+            left = arr
+        else:
+            right = arr
+    
+    for symbol in range(4, 0, -1):
+        ls = left.count(symbol)
+        rs = right.count(symbol)
+
+        if ls > rs:
+            print('A')
+            break
+        elif ls < rs:
+            print('B')
+            break
+        else:
+            continue
+    else:
+        print('D')
