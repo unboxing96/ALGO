371e61bb59af5f622bb1a133bfdc6bf98e613ac9
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-08-25 18:08:53 +0900

    [level 3] Title: 입국심사, Time: 723.77 ms, Memory: 14 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,51 +1,25 @@
-# 이해
-# 여러 개의 심사대에 심사관이 한 명씩 있다
-# 각 심사관의 소요 시간은 다르다
-# 대기하는 사람은 비어있는 심사대에서 심사를 받을 수 있다
-# 혹은 기다렸다가 갈 수도 있다. 즉시 가지 않아도 된다는 뜻 ?
-# 모든 심사를 완료하는 최소 시간을 return
-# n은 심사를 기다리는 사람 수
-# times는 각 심사위원의 속도
-
-# 풀이
-# 이분탐색
-# 피봇의 범위와, 판단 기준을 선택해야 한다.
-# 피봇의 범위는 총 소요 시간으로 해야겠다.
-# 총 소요 시간을 times의 각 원소로 나누면, 해당 시간 내 심사관이 처리 가능한 인원 수가 나온다.
-# 이 인원 수를 판단 기준으로 한다.
-
-# Dr.code
-# left = 1
-# right = max(times) * n
-# while left <= right:
-    # mid = (left + right) // 2
-    # people = 0
-    # for time in times:
-        # people += mid // time
+# 문제 분석
+# 소요 시간 m을 이분탐색하며 조건을 성립하는지 판단한다.
+# times의 원소를 time이라고 할 때, m // time 값을 모두 더한 것이 처리한 사람 수이다.
+# 처리한 사람의 수가 n명 이상이라면 m을 너무 크게 설정한 것이다. 범위를 줄인다.
+    # 조건은 달성했으니 answer를 갱신한다.
+# n 미만이라면 범위를 늘린다.
 
 def solution(n, times):
-    answer = int(2 ** 63 - 1)
-    left = 1
-    right = max(times) * n
+    answer = 0
     
-    while left <= right:
-        mid = (left + right) // 2
-        people = 0
+    start = min(times)
+    end = max(times) * n
+    
+    while start <= end:
+        m = (start + end) // 2
+        tmp_sum = 0
         for time in times:
-            people += mid // time
+            tmp_sum += m // time
+            
+        if tmp_sum >= n:
+            end = m - 1
+        else:
+            start = m + 1
     
-        if people >= n:
-            answer = min(answer, mid)
-            right = mid - 1
-
-        else: # people < n:
-            left = mid + 1
-        
-    return answer
-
-
-
-
-
-
-
+    return start
