5ccf9b720a0d18da9e9030e412e013c1fc5177f3
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-08-28 22:40:42 +0900

    [Silver III] Title: 1로 만들기, Time: 108 ms, Memory: 118248 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,12 +1,11 @@
 n = int(input())
-dp = [0] * (n + 1)
+dp = [0] * (n + 1) # i 위치를 1로 만들기 위해 필요한 연산 횟수
 
 for i in range(2, n + 1):
     dp[i] = dp[i - 1] + 1
-
-    if i % 2 == 0:
-        dp[i] = min(dp[i], dp[i // 2] + 1)
     if i % 3 == 0:
-        dp[i] = min(dp[i], dp[i // 3] + 1)
+        dp[i] = min(dp[i // 3] + 1, dp[i])
+    if i % 2 == 0:
+        dp[i] = min(dp[i // 2] + 1, dp[i])
 
 print(dp[n])
