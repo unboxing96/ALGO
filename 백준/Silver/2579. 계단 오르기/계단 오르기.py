2982dc367f3adc1e3a9935adc2061021a1baf927
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-08-29 22:03:50 +0900

    [Silver III] Title: 계단 오르기, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,17 +1,14 @@
+
 n = int(input())
-dp = [0] * (n + 1)
-stairs = [0] * (n + 1)
-for i in range(1, n+1):
-    stairs[i] = int(input())
-
+arr = [0] * 301
 for i in range(1, n + 1):
-    if i == 1:
-        dp[i] = stairs[i]
+    arr[i] = int(input())
 
-    elif i == 2:
-        dp[i] = stairs[i] + stairs[i-1]
-
-    else:
-        dp[i] = max(dp[i-3] + stairs[i-1] + stairs[i], dp[i-2] + stairs[i])
+dp = [0] * 301 # 해당 인덱스를 밟고 있는 경우, 해당 인덱스까지의 최댓값
+dp[1] = arr[1]
+dp[2] = arr[1] + arr[2]
+dp[3] = max(arr[1] + arr[3], arr[2] + arr[3])
+for i in range(4, n + 1):
+    dp[i] = max(arr[i] + arr[i - 1] + dp[i - 3], arr[i] + dp[i - 2])
 
 print(dp[n])
