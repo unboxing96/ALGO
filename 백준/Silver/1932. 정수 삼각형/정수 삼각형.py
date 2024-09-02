4078672ed8a67110f56dadf329e7db0a8816894f
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-09-02 11:34:20 +0900

    [Silver I] Title: 정수 삼각형, Time: 120 ms, Memory: 111368 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,14 @@
+n = int(input())
+matrix = [list(map(int, input().split())) for _ in range(n)]
+dp = matrix[:]
+
+for i in range(1, n):
+    for j in range(len(matrix[i])):
+        if j == 0:
+            dp[i][j] = dp[i][j] + dp[i - 1][j]
+        elif j == i:
+            dp[i][j] = dp[i][j] + dp[i - 1][j - 1]
+        else:
+            dp[i][j] = dp[i][j] + max(dp[i - 1][j] , dp[i - 1][j - 1])
+
+print(max(dp[n - 1]))
