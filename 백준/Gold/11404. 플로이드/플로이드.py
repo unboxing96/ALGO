5d7933bc012af60b719b8a08566631a4ab04412a
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-09-04 15:32:52 +0900

    [Gold IV] Title: 플로이드, Time: 184 ms, Memory: 111776 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,8 +1,7 @@
-import sys
 INF = int(1e9)
 
-n = int(sys.stdin.readline())  # 도시의 수
-m = int(sys.stdin.readline())  # 버스의 수
+n = int(input())  # 도시의 수
+m = int(input())  # 버스의 수
 
 graph = [[INF] * (n + 1) for _ in range(n + 1)]    # 모든 최단 거리를 저장
 for i in range(1, n + 1):
@@ -11,7 +10,7 @@ for i in range(1, n + 1):
             graph[i][j] = 0
 
 for _ in range(m):
-    a, b, c = map(int, sys.stdin.readline().split())
+    a, b, c = map(int, input().split())
     graph[a][b] = min(c, graph[a][b])   # 노선이 하나가 아닐 수 있음 > 최소값 넣기 
 
 # 2. 다이나믹 프로그래밍
