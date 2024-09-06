ab1defdc41dfb5f71b5363bee8d029450e7fae3b
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-09-06 10:50:14 +0900

    [Gold IV] Title: 최단경로, Time: 488 ms, Memory: 138808 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,45 +1,37 @@
-import sys
 import heapq
-
-input = sys.stdin.readline
-INF = int(1e9)
 
 v, e = map(int, input().split())
 start = int(input())
-
-# 인접 노드 그래프 초기화
+distance = [int(1e9)] * (v + 1)
+visited = [0] * (v + 1)
 graph = [[] for _ in range(v + 1)]
-for _ in range(e):
+for i in range(e):
     a, b, c = map(int, input().split())
-    graph[a].append((b, c))
-
-# 최단 거리 기록 리스트 초기화
-distance = [INF] * (v + 1)
-
+    graph[a].append((b, c)) # 노드 번호, 비용
 
 def dijkstra(start):
-    q = []  # 우선순위 큐 초기화
-    heapq.heappush(q, (0, start))  # start 노드 지정
-    distance[start] = 0  # 자기 자신으로의 거리는 0으로
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
 
     while q:
-        dist, now = heapq.heappop(q)  # 최소 거리가 가장 짧은 (노드) + 시작 지점부터 (최소 거리) pop
+        now_dist, now = heapq.heappop(q)
 
-        # 기존 최소 거리 < 이전 지점를 거친 최소 거리 -> 제외 (이미 방문했기 때문)
-        if distance[now] < dist:
+        # 현재 노드의 비용 > 최단 거리 테이블에 기록된 비용 -> 탐색할 필요가 없다.
+        if now_dist > distance[now]:
             continue
-
-        for i in graph[now]:
-            cost = dist + i[1]
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
-
+        
+        # 모든 노드의 인접 노드를 확인하며, 최단 거리 테이블 모두 갱신
+        for next, next_dist in graph[now]:
+            cost = now_dist + next_dist # now -> next로 거쳐가는 비용이
+            if cost < distance[next]: # next에 갱신된 비용보다 작다면
+                distance[next] = cost # 해당 비용으로 갱신
+                heapq.heappush(q, (cost, next))
 
 dijkstra(start)
 
 for i in range(1, v + 1):
-    if distance[i] == INF:
+    if distance[i] == int(1e9):
         print("INF")
     else:
         print(distance[i])
