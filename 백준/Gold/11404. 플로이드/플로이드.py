5cd0305f978452a728e996890b95c64756455fdb
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-09-06 15:22:09 +0900

    [Gold IV] Title: 플로이드, Time: 180 ms, Memory: 111772 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,28 +1,26 @@
 INF = int(1e9)
+n = int(input())
+m = int(input())
+dist = [[INF] * (n + 1) for _ in range(n + 1)]
 
-n = int(input())  # 도시의 수
-m = int(input())  # 버스의 수
-
-graph = [[INF] * (n + 1) for _ in range(n + 1)]    # 모든 최단 거리를 저장
 for i in range(1, n + 1):
     for j in range(1, n + 1):
         if i == j:
-            graph[i][j] = 0
+            dist[i][j] = 0
 
 for _ in range(m):
     a, b, c = map(int, input().split())
-    graph[a][b] = min(c, graph[a][b])   # 노선이 하나가 아닐 수 있음 > 최소값 넣기 
+    dist[a][b] = min(dist[a][b], c)
 
-# 2. 다이나믹 프로그래밍
 for k in range(1, n + 1):
-    for a in range(1, n + 1):
-        for b in range(1, n + 1):
-            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
 
-for a in range(1, n + 1):
-    for b in range(1, n + 1):
-        if graph[a][b] == INF:
-            print("0",  end=" ")
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if dist[i][j] == INF:
+            print(0, end=" ")
         else:
-            print(graph[a][b], end=" ")
+            print(dist[i][j], end=" ")
     print()
