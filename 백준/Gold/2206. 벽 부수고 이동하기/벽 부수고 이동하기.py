a82c518e90533e4a9bee462ea44f3b13b2493934
taehyun kim <102353544+unboxing96@users.noreply.github.com>
2024-10-05 13:54:36 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1432 ms, Memory: 277376 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,43 +1,37 @@
 from collections import deque
 
-n, m = map(int, input().split())
-graph = []
+n, m = map(int, input().split()) # 실제로는 -1 해서 사용
+matrix = [list(map(int, input())) for _ in range(n)]
+visited = [[[0] * 2 for _ in range(m)] for _ in range(n)] # visited[x][y][0]은 벽 파괴 가능, visited[x][y][1]은 벽 파괴 불가능
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
-# 3차원 행렬을 통해 벽의 파괴를 파악함. visited[x][y][0]은 벽 파괴 가능. [x][y][1]은 불가능.
-visited = [[[0] * 2 for _ in range(m)] for _ in range(n)]
-visited[0][0][0] = 1
+def bfs():
+    q = deque()
+    q.append((0, 0, 0)) # x, y, is_break_done
+    visited[0][0][0] = 1
 
-for i in range(n):
-    graph.append(list(map(int, input())))
-
-# 상하좌우
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
-
-
-def bfs(x, y, z):
-    queue = deque()
-    queue.append((x, y, z))
-
-    while queue:
-        a, b, c = queue.popleft()
-        # 끝 점에 도달하면 이동 횟수를 출력
-        if a == n - 1 and b == m - 1:
-            return visited[a][b][c]
+    while q:
+        x, y, c = q.popleft()
+        if x == n - 1 and y == m - 1:
+            return visited[x][y][c]
+        
         for i in range(4):
-            nx = a + dx[i]
-            ny = b + dy[i]
+            nx = x + dx[i]
+            ny = y + dy[i]
+
             if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
-            # 다음 이동할 곳이 벽이고, 벽파괴기회를 사용하지 않은 경우
-            if graph[nx][ny] == 1 and c == 0 :
-                visited[nx][ny][1] = visited[a][b][0] + 1
-                queue.append((nx, ny, 1))
-            # 다음 이동할 곳이 벽이 아니고, 아직 한 번도 방문하지 않은 곳이면
-            elif graph[nx][ny] == 0 and visited[nx][ny][c] == 0:
-                visited[nx][ny][c] = visited[a][b][c] + 1
-                queue.append((nx, ny, c))
+            # 다음 이동할 곳이 벽이고, 아직 벽 파괴 기회가 남아있는 경우 -> 파괴 !
+            if matrix[nx][ny] == 1 and c == 0:
+                visited[nx][ny][1] = visited[x][y][c] + 1
+                q.append((nx, ny, 1))
+
+            # 다음 이동할 곳이 벽이 아니고, 방문하지 않은 경우
+            elif matrix[nx][ny] == 0 and visited[nx][ny][c] == 0:
+                visited[nx][ny][c] = visited[x][y][c] + 1
+                q.append((nx, ny, c))
+    
     return -1
 
-
-print(bfs(0, 0, 0))
+print(bfs())
